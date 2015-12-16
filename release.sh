5d634418aa6c9376732fde34aa46d008ff41614d
Rémy-Christophe Schermesser <git@schermesser.com>
2015-12-16 18:40:16 +0100

    Automatic releases \o/

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 mvn -Darguments="-DskipTests" release:prepare
 mvn -Darguments="-DskipTests" release:perform
+
+REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f2`
+
+mvn nexus-staging:close -DstagingRepositoryId="$REP_ID"
+mvn nexus-staging:release -DstagingRepositoryId="$REP_IDgi
