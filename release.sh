18c054214dad0c32ae9d141c0c08f9a1b46284b0
Rémy-Christophe Schermesser <git@schermesser.com>
2016-01-04 16:32:14 +0100

    Fix release file

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ mvn -Darguments="-DskipTests" release:perform
 REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f2`
 
 mvn nexus-staging:close -DstagingRepositoryId="$REP_ID"
-mvn nexus-staging:release -DstagingRepositoryId="$REP_IDgi
+mvn nexus-staging:release -DstagingRepositoryId="$REP_ID"
