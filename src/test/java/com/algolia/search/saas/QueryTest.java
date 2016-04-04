c6b92f8918c48fe9ef94c545be29202e36e60deb
Paul-Louis NECH <pln@algolia.com>
2016-04-04 18:18:52 +0200

    Tests: Added unit-tests for around parameters

--- src/test/java/com/algolia/search/saas/QueryTest.java
+++ src/test/java/com/algolia/search/saas/QueryTest.java
@@ -1,5 +1,6 @@
 package com.algolia.search.saas;
 
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,12 +12,38 @@ import static org.junit.Assert.assertTrue;
 @RunWith(JUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class QueryTest {
+    private Query q;
+
+    @Before
+    public void setUp() {
+        q = new Query();
+    }
 
     @Test
     public void hitsPerPageEquals20() {
-        Query q0 = new Query();
-        q0.setHitsPerPage(20);
-        String qs = q0.getQueryString();
+        q.setHitsPerPage(20);
+        String qs = q.getQueryString();
         assertTrue(qs.contains("hitsPerPage=20"));
+    }
+
+    @Test
+    public void hasAroundRadius() {
+        q.setAroundRadius(42);
+        String qs = q.getQueryString();
+        assertTrue(qs.contains("aroundRadius=42"));
+    }
+
+    @Test
+    public void hasAroundPrecision() {
+        q.setAroundPrecision(42);
+        String qs = q.getQueryString();
+        assertTrue(qs.contains("aroundPrecision=42"));
+    }
+
+    @Test
+    public void hasMinimumAroundRadius() {
+        q.setMinimumAroundRadius(42);
+        String qs = q.getQueryString();
+        assertTrue(qs.contains("minimumAroundRadius=42"));
     }
 }
