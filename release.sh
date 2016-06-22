bbc9618dbe791e5de6a080fa12218e8867e83098
Rémy-Christophe Schermesser <git@schermesser.com>
2016-06-22 16:20:49 +0200

    Better release file

--- release.sh
+++ release.sh
@@ -4,5 +4,17 @@ mvn -Darguments="-DskipTests" release:perform
 
 REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f2`
 
+if [ -z "$REP_ID" ]; then
+	# Retrying
+	REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f1`
+fi
+
+if [ -z "$REP_ID" ]; then
+	echo "Can not find a REP_ID"
+	exit 1
+fi
+
+echo "REP_ID found: $REP_ID"
+
 mvn nexus-staging:close -DstagingRepositoryId="$REP_ID"
 mvn nexus-staging:release -DstagingRepositoryId="$REP_ID"
