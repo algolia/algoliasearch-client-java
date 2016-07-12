ee7908d9626e3a3c984710b628becd08616e0936
Rémy-Christophe Schermesser <git@schermesser.com>
2016-07-12 11:47:25 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -1,22 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 mvn -Darguments="-DskipTests" release:prepare
 mvn -Darguments="-DskipTests" release:perform
 
 sleep 5
 
-REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f2`
-
-if [ -z "$REP_ID" ]; then
-	# Retrying
-	REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f1`
-fi
+REP_ID=`mvn nexus-staging:rc-list | grep comalgolia | cut -d" " -f1`
 
 if [ -z "$REP_ID" ]; then
 	echo "Can not find a REP_ID"
 	exit 1
 fi
 
+echo "----------------------"
 echo "REP_ID found: $REP_ID"
+echo "----------------------"
 
 mvn nexus-staging:close -DstagingRepositoryId="$REP_ID"
 mvn nexus-staging:release -DstagingRepositoryId="$REP_ID"
