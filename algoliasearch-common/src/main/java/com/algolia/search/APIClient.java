adamretter/algoliasearch-client-java-2

feat(query_rules): Add query rules

algoliasearch-common/src/main/java/com/algolia/search/APIClient.java
@@ -11,6 +11,7 @@ import com.algolia.search.inputs.batch.BatchDeleteObjectOperation;
 import com.algolia.search.inputs.batch.BatchPartialUpdateObjectOperation;
 import com.algolia.search.inputs.batch.BatchUpdateObjectOperation;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
+import com.algolia.search.inputs.query_rules.Rule;
 import com.algolia.search.inputs.synonym.AbstractSynonym;
 import com.algolia.search.objects.*;
 import com.algolia.search.objects.tasks.sync.*;
@@ -833,6 +834,83 @@ public class APIClient {
       .setData(new Search(query));
 
     return httpClient.requestWithRetry(algoliaRequest);
+  }
+
+  Task saveRule(String indexName, String ruleId, Rule queryRule, Boolean forwardToReplicas) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.PUT,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", ruleId),
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString())).setData(queryRule)
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  Optional<Rule> getRule(String indexName, String queryRulesID) throws AlgoliaException {
+    return Optional.ofNullable(
+      httpClient.requestWithRetry(
+        new AlgoliaRequest<>(
+          HttpMethod.GET,
+          false,
+          Arrays.asList("1", "indexes", indexName, "rules", queryRulesID),
+          Rule.class
+        )
+      )
+    );
+  }
+
+  Task deleteRule(String indexName, String queryRulesID, Boolean forwardToReplicas) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.DELETE,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", queryRulesID),
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString()))
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  Task clearRules(String indexName, Boolean forwardToReplicas) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "clear"),
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString()))
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  SearchRuleResult searchRules(String indexName, RuleQuery query) throws AlgoliaException {
+    return httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "search"),
+        SearchRuleResult.class).setData(query)
+    );
+  }
+
+  Task batchRules(String indexName, List<Rule> queryRules, Boolean forwardToReplicas, Boolean replaceExistingQueryRules) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "batch"),
+        Task.class
+      )
+        .setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString(), "replaceExistingQueryRules", replaceExistingQueryRules.toString()))
+        .setData(queryRules)
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
   }
 
   /**

algoliasearch-common/src/main/java/com/algolia/search/AsyncIndex.java
@@ -2,11 +2,9 @@ package com.algolia.search;
 
 import com.algolia.search.inputs.BatchOperation;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
+import com.algolia.search.inputs.query_rules.Rule;
 import com.algolia.search.inputs.synonym.AbstractSynonym;
-import com.algolia.search.objects.ApiKey;
-import com.algolia.search.objects.IndexSettings;
-import com.algolia.search.objects.Query;
-import com.algolia.search.objects.SynonymQuery;
+import com.algolia.search.objects.*;
 import com.algolia.search.objects.tasks.async.AsyncTask;
 import com.algolia.search.objects.tasks.async.AsyncTaskIndexing;
 import com.algolia.search.objects.tasks.async.AsyncTaskSingleIndex;
@@ -211,7 +209,7 @@ interface AsyncSettings<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Set settings of this index, and do not forward to slaves
+   * Set settings of this index, and do not forward to replicas
    *
    * @param settings the settings to set
    * @return the related AsyncTask
@@ -224,7 +222,7 @@ interface AsyncSettings<T> extends BaseAsyncIndex<T> {
    * Set settings of this index
    *
    * @param settings          the settings to set
-   * @param forwardToReplicas should these updates be forwarded to the slaves
+   * @param forwardToReplicas should these updates be forwarded to the replicas
    * @return the related AsyncTask
    */
   default CompletableFuture<AsyncTask> setSettings(@Nonnull IndexSettings settings, @Nonnull Boolean forwardToReplicas) {
@@ -427,7 +425,7 @@ interface AsyncPartialUpdate<T> extends BaseAsyncIndex<T> {
 interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
 
   /**
-   * Saves/updates a synonym without replacing it and NOT forwarding it to the slaves
+   * Saves/updates a synonym without replacing it and NOT forwarding it to the replicas
    *
    * @param synonymID the id of the synonym
    * @param content   the synonym
@@ -442,7 +440,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    *
    * @param synonymID         the id of the synonym
    * @param content           the synonym
-   * @param forwardToReplicas should this request be forwarded to slaves
+   * @param forwardToReplicas should this request be forwarded to replicas
    * @return the associated task
    */
   default CompletableFuture<AsyncTask> saveSynonym(@Nonnull String synonymID, @Nonnull AbstractSynonym content, boolean forwardToReplicas) {
@@ -454,7 +452,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    *
    * @param synonymID               the id of the synonym
    * @param content                 the synonym
-   * @param forwardToReplicas       should this request be forwarded to slaves
+   * @param forwardToReplicas       should this request be forwarded to replicas
    * @param replaceExistingSynonyms should replace if this synonyms exists
    * @return the associated task
    */
@@ -473,7 +471,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Deletes a synonym by ID and NOT forwarding it to the slaves
+   * Deletes a synonym by ID and NOT forwarding it to the replicas
    *
    * @param synonymID the id of the synonym
    * @return the associated task
@@ -486,7 +484,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    * Deletes a synonym
    *
    * @param synonymID         the id of the synonym
-   * @param forwardToReplicas should this request be forwarded to slaves
+   * @param forwardToReplicas should this request be forwarded to replicas
    * @return the associated task
    */
   default CompletableFuture<AsyncTask> deleteSynonym(@Nonnull String synonymID, boolean forwardToReplicas) {
@@ -494,7 +492,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Clear all synonyms and NOT forwarding it to the slaves
+   * Clear all synonyms and NOT forwarding it to the replicas
    *
    * @return the associated task
    */
@@ -545,13 +543,132 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Add or Replace a list of synonyms, no forward to slaves, and no replacement
+   * Add or Replace a list of synonyms, no forward to replicas, and no replacement
    *
    * @param synonyms List of synonyms
    * @return the associated task
    */
   default CompletableFuture<AsyncTask> batchSynonyms(@Nonnull List<AbstractSynonym> synonyms) {
     return batchSynonyms(synonyms, false, false);
+  }
+
+}
+
+interface AsyncRules<T> extends BaseAsyncIndex<T> {
+
+  /**
+   * Saves/updates a rule without replacing it and NOT forwarding it to the replicas
+   *
+   * @param ruleID the id of the query rule
+   * @param rule   the query rule
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule) {
+    return saveRule(ruleID, rule, false);
+  }
+
+  /**
+   * Saves/updates a rule
+   *
+   * @param ruleID            the id of the query rule
+   * @param rule              the query rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule, boolean forwardToReplicas) {
+    return getApiClient().saveRule(getName(), ruleID, rule, forwardToReplicas);
+  }
+
+  /**
+   * Get a rule by ID
+   *
+   * @param ruleID the id of the rule
+   * @return the associated task
+   */
+  default CompletableFuture<Optional<Rule>> getRule(@Nonnull String ruleID) {
+    return getApiClient().getRule(getName(), ruleID);
+  }
+
+  /**
+   * Deletes a rule by ID and NOT forwarding it to the replicas
+   *
+   * @param ruleID the id of the query rule
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID) {
+    return deleteRule(ruleID, false);
+  }
+
+  /**
+   * Deletes a rule
+   *
+   * @param ruleID            the id of the rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID, boolean forwardToReplicas) {
+    return getApiClient().deleteRule(getName(), ruleID, forwardToReplicas);
+  }
+
+  /**
+   * Clear all rules and NOT forwarding it to the replicas
+   *
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules() {
+    return clearRules(false);
+  }
+
+  /**
+   * Clears all rules
+   *
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules(boolean forwardToReplicas) {
+    return getApiClient().clearRules(getName(), forwardToReplicas);
+  }
+
+  /**
+   * Search for rules
+   *
+   * @param query the query
+   * @return the results of the query
+   */
+  default CompletableFuture<SearchRuleResult> searchRules(@Nonnull RuleQuery query) {
+    return getApiClient().searchRules(getName(), query);
+  }
+
+  /**
+   * Add or replace a list of rules
+   *
+   * @param rules              List of rules
+   * @param forwardToReplicas  Forward the operation to the replicas indices
+   * @param clearExistingRules Replace the existing rules with this batch
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas, boolean clearExistingRules) {
+    return getApiClient().batchRules(getName(), rules, forwardToReplicas, clearExistingRules);
+  }
+
+  /**
+   * Add or Replace a list of rules, no replacement
+   *
+   * @param rules             List of rules
+   * @param forwardToReplicas Forward the operation to the slave indices
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas) {
+    return batchRules(rules, forwardToReplicas, false);
+  }
+
+  /**
+   * Add or Replace a list of rules, no forward to replicas, and no replacement
+   *
+   * @param rules List of rules
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules) {
+    return batchRules(rules, false, false);
   }
 
 }
@@ -565,7 +682,8 @@ public class AsyncIndex<T> implements
   AsyncKey<T>,
   AsyncSearchForFacet<T>,
   AsyncPartialUpdate<T>,
-  AsyncSynonyms<T> {
+  AsyncSynonyms<T>,
+  AsyncRules<T> {
 
   /**
    * Index name

algolia/algoliasearch-client-java

algoliasearch-common/src/main/java/com/algolia/search/APIClient.java
@@ -11,6 +11,7 @@ import com.algolia.search.inputs.batch.BatchDeleteObjectOperation;
 import com.algolia.search.inputs.batch.BatchPartialUpdateObjectOperation;
 import com.algolia.search.inputs.batch.BatchUpdateObjectOperation;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
+import com.algolia.search.inputs.query_rules.Rule;
 import com.algolia.search.inputs.synonym.AbstractSynonym;
 import com.algolia.search.objects.*;
 import com.algolia.search.objects.tasks.sync.*;
@@ -804,7 +805,7 @@ public class APIClient {
       klass
     );
 
-    SearchResult<T> result = httpClient.requestWithRetry(algoliaRequest.setData(new Search(query)));
+    SearchResult<T> result = httpClient.requestWithRetry(algoliaRequest.setData(query));
     if (result == null) { //Special case when the index does not exists
       throw new AlgoliaIndexNotFoundException(indexName + " does not exist");
     }
@@ -980,14 +981,14 @@ public class APIClient {
   @SuppressWarnings("unchecked")
   <T> BrowseResult<T> browse(String indexName, Query query, String cursor, Class<T> klass, RequestOptions requestOptions) throws AlgoliaException {
     AlgoliaRequest<BrowseResult> algoliaRequest = new AlgoliaRequest<>(
-      HttpMethod.GET,
+      HttpMethod.POST,
       true,
       Arrays.asList("1", "indexes", indexName, "browse"),
       requestOptions,
       BrowseResult.class,
       klass
     )
-      .setParameters(query.setCursor(cursor).toQueryParam());
+      .setData(query.setCursor(cursor));
 
     return httpClient.requestWithRetry(algoliaRequest);
   }
@@ -1002,9 +1003,92 @@ public class APIClient {
       requestOptions,
       SearchFacetResult.class
     )
-      .setData(new Search(query));
+      .setData(query);
 
     return httpClient.requestWithRetry(algoliaRequest);
+  }
+
+  Task saveRule(String indexName, String ruleId, Rule queryRule, Boolean forwardToReplicas, RequestOptions requestOptions) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.PUT,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", ruleId),
+        requestOptions,
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString())).setData(queryRule)
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  Optional<Rule> getRule(String indexName, String queryRulesID, RequestOptions requestOptions) throws AlgoliaException {
+    return Optional.ofNullable(
+      httpClient.requestWithRetry(
+        new AlgoliaRequest<>(
+          HttpMethod.GET,
+          false,
+          Arrays.asList("1", "indexes", indexName, "rules", queryRulesID),
+          requestOptions,
+          Rule.class
+        )
+      )
+    );
+  }
+
+  Task deleteRule(String indexName, String queryRulesID, Boolean forwardToReplicas, RequestOptions requestOptions) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.DELETE,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", queryRulesID),
+        requestOptions,
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString()))
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  Task clearRules(String indexName, Boolean forwardToReplicas, RequestOptions requestOptions) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "clear"),
+        requestOptions,
+        Task.class
+      ).setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString()))
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  SearchRuleResult searchRules(String indexName, RuleQuery query, RequestOptions requestOptions) throws AlgoliaException {
+    return httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "search"),
+        requestOptions,
+        SearchRuleResult.class).setData(query)
+    );
+  }
+
+  Task batchRules(String indexName, List<Rule> queryRules, Boolean forwardToReplicas, Boolean clearExistingRules, RequestOptions requestOptions) throws AlgoliaException {
+    Task task = httpClient.requestWithRetry(
+      new AlgoliaRequest<>(
+        HttpMethod.POST,
+        false,
+        Arrays.asList("1", "indexes", indexName, "rules", "batch"),
+        requestOptions,
+        Task.class
+      )
+        .setParameters(ImmutableMap.of("forwardToReplicas", forwardToReplicas.toString(), "clearExistingRules", clearExistingRules.toString()))
+        .setData(queryRules)
+    );
+
+    return task.setAPIClient(this).setIndex(indexName);
   }
 
   /**

algoliasearch-common/src/main/java/com/algolia/search/AsyncIndex.java
@@ -2,6 +2,7 @@ package com.algolia.search;
 
 import com.algolia.search.inputs.BatchOperation;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
+import com.algolia.search.inputs.query_rules.Rule;
 import com.algolia.search.inputs.synonym.AbstractSynonym;
 import com.algolia.search.objects.*;
 import com.algolia.search.objects.tasks.async.AsyncTask;
@@ -394,7 +395,7 @@ interface AsyncSettings<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Set settings of this index, and do not forward to slaves
+   * Set settings of this index, and do not forward to replicas
    *
    * @param settings the settings to set
    * @return the related AsyncTask
@@ -418,7 +419,7 @@ interface AsyncSettings<T> extends BaseAsyncIndex<T> {
    * Set settings of this index
    *
    * @param settings          the settings to set
-   * @param forwardToReplicas should these updates be forwarded to the slaves
+   * @param forwardToReplicas should these updates be forwarded to the replicas
    * @return the related AsyncTask
    */
   default CompletableFuture<AsyncTask> setSettings(@Nonnull IndexSettings settings, @Nonnull Boolean forwardToReplicas) {
@@ -761,7 +762,7 @@ interface AsyncPartialUpdate<T> extends BaseAsyncIndex<T> {
 interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
 
   /**
-   * Saves/updates a synonym without replacing it and NOT forwarding it to the slaves
+   * Saves/updates a synonym without replacing it and NOT forwarding it to the replicas
    *
    * @param synonymID the id of the synonym
    * @param content   the synonym
@@ -788,7 +789,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    *
    * @param synonymID         the id of the synonym
    * @param content           the synonym
-   * @param forwardToReplicas should this request be forwarded to slaves
+   * @param forwardToReplicas should this request be forwarded to replicas
    * @return the associated task
    */
   default CompletableFuture<AsyncTask> saveSynonym(@Nonnull String synonymID, @Nonnull AbstractSynonym content, boolean forwardToReplicas) {
@@ -813,7 +814,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    *
    * @param synonymID               the id of the synonym
    * @param content                 the synonym
-   * @param forwardToReplicas       should this request be forwarded to slaves
+   * @param forwardToReplicas       should this request be forwarded to replicas
    * @param replaceExistingSynonyms should replace if this synonyms exists
    * @return the associated task
    */
@@ -857,7 +858,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Deletes a synonym by ID and NOT forwarding it to the slaves
+   * Deletes a synonym by ID and NOT forwarding it to the replicas
    *
    * @param synonymID the id of the synonym
    * @return the associated task
@@ -881,7 +882,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
    * Deletes a synonym
    *
    * @param synonymID         the id of the synonym
-   * @param forwardToReplicas should this request be forwarded to slaves
+   * @param forwardToReplicas should this request be forwarded to replicas
    * @return the associated task
    */
   default CompletableFuture<AsyncTask> deleteSynonym(@Nonnull String synonymID, boolean forwardToReplicas) {
@@ -901,7 +902,7 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
   }
 
   /**
-   * Clear all synonyms and NOT forwarding it to the slaves
+   * Clear all synonyms and NOT forwarding it to the replicas
    *
    * @return the associated task
    */
@@ -1030,6 +1031,251 @@ interface AsyncSynonyms<T> extends BaseAsyncIndex<T> {
 
 }
 
+interface AsyncRules<T> extends BaseAsyncIndex<T> {
+
+  /**
+   * Saves/updates a rule without replacing it and NOT forwarding it to the replicas
+   *
+   * @param ruleID the id of the query rule
+   * @param rule   the query rule
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule) {
+    return saveRule(ruleID, rule, false);
+  }
+
+  /**
+   * Saves/updates a rule without replacing it and NOT forwarding it to the replicas
+   *
+   * @param ruleID         the id of the query rule
+   * @param rule           the query rule
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule, @Nonnull RequestOptions requestOptions) {
+    return saveRule(ruleID, rule, false, requestOptions);
+  }
+
+  /**
+   * Saves/updates a rule
+   *
+   * @param ruleID            the id of the query rule
+   * @param rule              the query rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule, boolean forwardToReplicas) {
+    return saveRule(ruleID, rule, forwardToReplicas, RequestOptions.empty);
+  }
+
+  /**
+   * Saves/updates a rule
+   *
+   * @param ruleID            the id of the query rule
+   * @param rule              the query rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @param requestOptions    Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> saveRule(@Nonnull String ruleID, @Nonnull Rule rule, boolean forwardToReplicas, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().saveRule(getName(), ruleID, rule, forwardToReplicas, requestOptions);
+  }
+
+  /**
+   * Get a rule by ID
+   *
+   * @param ruleID the id of the rule
+   * @return the associated task
+   */
+  default CompletableFuture<Optional<Rule>> getRule(@Nonnull String ruleID) {
+    return getRule(ruleID, RequestOptions.empty);
+  }
+
+  /**
+   * Get a rule by ID
+   *
+   * @param ruleID         the id of the rule
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<Optional<Rule>> getRule(@Nonnull String ruleID, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().getRule(getName(), ruleID, requestOptions);
+  }
+
+  /**
+   * Deletes a rule by ID and NOT forwarding it to the replicas
+   *
+   * @param ruleID the id of the query rule
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID) {
+    return deleteRule(ruleID, false);
+  }
+
+  /**
+   * Deletes a rule by ID and NOT forwarding it to the replicas
+   *
+   * @param ruleID         the id of the query rule
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID, @Nonnull RequestOptions requestOptions) {
+    return deleteRule(ruleID, false, requestOptions);
+  }
+
+  /**
+   * Deletes a rule
+   *
+   * @param ruleID            the id of the rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID, boolean forwardToReplicas) {
+    return deleteRule(ruleID, forwardToReplicas, RequestOptions.empty);
+  }
+
+  /**
+   * Deletes a rule
+   *
+   * @param ruleID            the id of the rule
+   * @param forwardToReplicas should this request be forwarded to replicas
+   * @param requestOptions    Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> deleteRule(@Nonnull String ruleID, boolean forwardToReplicas, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().deleteRule(getName(), ruleID, forwardToReplicas, requestOptions);
+  }
+
+  /**
+   * Clear all Rules and NOT forwarding it to the replicas
+   *
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules() {
+    return clearRules(false);
+  }
+
+  /**
+   * Clear all Rules and NOT forwarding it to the replicas
+   *
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules(@Nonnull RequestOptions requestOptions) {
+    return clearRules(false, requestOptions);
+  }
+
+  /**
+   * Clears all Rules
+   *
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules(boolean forwardToReplicas) {
+    return clearRules(forwardToReplicas, RequestOptions.empty);
+  }
+
+  /**
+   * Clears all Rules
+   *
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> clearRules(boolean forwardToReplicas, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().clearRules(getName(), forwardToReplicas, requestOptions);
+  }
+
+  /**
+   * Search for Rules
+   *
+   * @param query the query
+   * @return the results of the query
+   */
+  default CompletableFuture<SearchRuleResult> searchRules(@Nonnull RuleQuery query) {
+    return searchRules(query, RequestOptions.empty);
+  }
+
+  /**
+   * Search for Rules
+   *
+   * @param requestOptions Options to pass to this request
+   * @param query          the query
+   * @return the results of the query
+   */
+  default CompletableFuture<SearchRuleResult> searchRules(@Nonnull RuleQuery query, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().searchRules(getName(), query, requestOptions);
+  }
+
+  /**
+   * Add or replace a list of Rules
+   *
+   * @param rules              List of Rules
+   * @param forwardToReplicas  Forward the operation to the replicas indices
+   * @param clearExistingRules Replace the existing Rules with this batch
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas, boolean clearExistingRules) {
+    return batchRules(rules, forwardToReplicas, clearExistingRules, RequestOptions.empty);
+  }
+
+  /**
+   * Add or replace a list of Rules
+   *
+   * @param rules              List of Rules
+   * @param forwardToReplicas  Forward the operation to the replicas indices
+   * @param clearExistingRules Replace the existing Rules with this batch
+   * @param requestOptions     Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas, boolean clearExistingRules, @Nonnull RequestOptions requestOptions) {
+    return getApiClient().batchRules(getName(), rules, forwardToReplicas, clearExistingRules, requestOptions);
+  }
+
+  /**
+   * Add or Replace a list of Rules, no replacement
+   *
+   * @param rules             List of Rules
+   * @param forwardToReplicas Forward the operation to the slave indices
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas) {
+    return batchRules(rules, forwardToReplicas, false);
+  }
+
+  /**
+   * Add or Replace a list of Rules, no replacement
+   *
+   * @param rules             List of Rules
+   * @param forwardToReplicas Forward the operation to the slave indices
+   * @param requestOptions    Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, boolean forwardToReplicas, @Nonnull RequestOptions requestOptions) {
+    return batchRules(rules, forwardToReplicas, false, requestOptions);
+  }
+
+  /**
+   * Add or Replace a list of Rules, no forward to replicas, and no replacement
+   *
+   * @param rules List of Rules
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules) {
+    return batchRules(rules, false, false);
+  }
+
+  /**
+   * Add or Replace a list of Rules, no forward to replicas, and no replacement
+   *
+   * @param rules          List of Rules
+   * @param requestOptions Options to pass to this request
+   * @return the associated task
+   */
+  default CompletableFuture<AsyncTask> batchRules(@Nonnull List<Rule> rules, @Nonnull RequestOptions requestOptions) {
+    return batchRules(rules, false, false, requestOptions);
+  }
+
+}
+
 @SuppressWarnings("WeakerAccess")
 public class AsyncIndex<T> implements
   AsyncIndexCRUD<T>,
@@ -1039,7 +1285,8 @@ public class AsyncIndex<T> implements
   AsyncKey<T>,
   AsyncSearchForFacet<T>,
   AsyncPartialUpdate<T>,
-  AsyncSynonyms<T> {
+  AsyncSynonyms<T>,
+  AsyncRules<T> {
 
   /**
    * Index name