c08bd46fdd6789fb7f37d46e8e0ce1e45bb13d36
Rémy-Christophe Schermesser <git@schermesser.com>
2017-11-07 11:39:03 +0100

    feat(sync): Add wait for task with requests options (#393)

--- algoliasearch-common/src/main/java/com/algolia/search/objects/tasks/sync/Task.java
+++ algoliasearch-common/src/main/java/com/algolia/search/objects/tasks/sync/Task.java
@@ -2,8 +2,10 @@ package com.algolia.search.objects.tasks.sync;
 
 import com.algolia.search.APIClient;
 import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.objects.RequestOptions;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import javax.annotation.Nonnull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -20,7 +22,7 @@ public class Task extends GenericTask<Long> {
    * @throws AlgoliaException
    */
   public void waitForCompletion() throws AlgoliaException {
-    apiClient.waitTask(this, 100);
+    waitForCompletion(RequestOptions.empty);
   }
 
   /**
@@ -30,7 +32,29 @@ public class Task extends GenericTask<Long> {
    * @throws AlgoliaException
    */
   public void waitForCompletion(long timeToWait) throws AlgoliaException {
-    apiClient.waitTask(this, timeToWait);
+    waitForCompletion(timeToWait, RequestOptions.empty);
+  }
+
+  /**
+   * Wait for the completion of this task
+   *
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaException
+   */
+  public void waitForCompletion(@Nonnull RequestOptions requestOptions) throws AlgoliaException {
+    waitForCompletion(100, requestOptions);
+  }
+
+  /**
+   * Wait for the completion of this task
+   *
+   * @param timeToWait the time to wait in milliseconds
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaException
+   */
+  public void waitForCompletion(long timeToWait, @Nonnull RequestOptions requestOptions)
+      throws AlgoliaException {
+    apiClient.waitTask(this, timeToWait, requestOptions);
   }
 
   @Override
