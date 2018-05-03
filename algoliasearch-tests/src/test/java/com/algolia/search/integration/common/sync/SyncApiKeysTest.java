7adbb1cce51601a39f37288745d3ecdfa6728fe8
Julien Bourdeau <julien@sigerr.org>
2018-05-03 19:17:52 +0200

    chore(travis): Bypass key management tests for community PRs

--- algoliasearch-tests/src/test/java/com/algolia/search/integration/common/sync/SyncApiKeysTest.java
+++ algoliasearch-tests/src/test/java/com/algolia/search/integration/common/sync/SyncApiKeysTest.java
@@ -1,6 +1,7 @@
 package com.algolia.search.integration.common.sync;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 
 import com.algolia.search.AlgoliaObject;
 import com.algolia.search.Index;
@@ -9,10 +10,23 @@ import com.algolia.search.exceptions.AlgoliaException;
 import com.algolia.search.objects.ApiKey;
 import java.util.Collections;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 
 @SuppressWarnings("ConstantConditions")
 public abstract class SyncApiKeysTest extends SyncAlgoliaIntegrationTest {
+
+  /**
+   * If the tests are run on Travis from a pull request of a contributor we need to bypass key
+   * management tests since they require the Admin API key
+   */
+  @Before
+  public void skipIfIsCommunity() {
+    String isCommunity = System.getenv("IS_COMMUNITY");
+    if (isCommunity != null && !isCommunity.isEmpty()) {
+      assumeTrue(false);
+    }
+  }
 
   private void waitForKeyPresent(Index<AlgoliaObject> index, String description)
       throws AlgoliaException, InterruptedException {
