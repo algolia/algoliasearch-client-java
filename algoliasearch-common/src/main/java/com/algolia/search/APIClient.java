cmiller-wikia/algoliasearch-client-java-2

feat: Implement AB Testing

algoliasearch-common/src/main/java/com/algolia/search/APIClient.java
@@ -7,6 +7,7 @@ import com.algolia.search.http.AlgoliaRequest;
 import com.algolia.search.http.AlgoliaRequestKind;
 import com.algolia.search.http.HttpMethod;
 import com.algolia.search.inputs.*;
+import com.algolia.search.inputs.analytics.ABTest;
 import com.algolia.search.inputs.batch.*;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
 import com.algolia.search.inputs.query_rules.Rule;
@@ -1377,6 +1378,61 @@ public class APIClient {
                 .setData(queryRules));
 
     return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  public TaskABTest AddABTest(ABTest abtest) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.POST,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                TaskABTest.class)
+            .setData(abtest));
+  }
+
+  public TaskABTest StopABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.POST,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id), "stop"),
+            RequestOptions.empty,
+            TaskABTest.class));
+  }
+
+  public TaskABTest DeleteABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.DELETE,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            TaskABTest.class));
+  }
+
+  public ABTest GetABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.GET,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            ABTest.class));
+  }
+
+  public ABTests GetABTests(int offset, int limit) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.GET,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                ABTests.class)
+            .setParameters(
+                ImmutableMap.of(
+                    "offset", Integer.toString(offset),
+                    "limit", Integer.toString(limit))));
   }
 
   /** Used internally for deleteByQuery */

algoliasearch-common/src/main/java/com/algolia/search/Analytics.java
@@ -1,10 +1,40 @@
 package com.algolia.search;
 
-public class Analytics {
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.inputs.analytics.ABTest;
+import com.algolia.search.objects.tasks.sync.TaskABTest;
+import com.algolia.search.responses.ABTests;
+import java.io.Serializable;
+
+public class Analytics implements Serializable {
 
   private APIClient client;
 
   Analytics(APIClient client) {
     this.client = client;
+  }
+
+  public TaskABTest AddABTest(ABTest abtest) throws AlgoliaException {
+    return client.AddABTest(abtest);
+  }
+
+  public TaskABTest StopABTest(long id) throws AlgoliaException {
+    return client.StopABTest(id);
+  }
+
+  public TaskABTest DeleteABTest(long id) throws AlgoliaException {
+    return client.DeleteABTest(id);
+  }
+
+  public ABTest GetABTest(long id) throws AlgoliaException {
+    return client.GetABTest(id);
+  }
+
+  public ABTests GetABTests(int offset, int limit) throws AlgoliaException {
+    return client.GetABTests(offset, limit);
+  }
+
+  public void WaitTask(TaskABTest task) throws AlgoliaException {
+    this.client.waitTask(task);
   }
 }

algoliasearch-common/src/main/java/com/algolia/search/AsyncAPIClient.java
@@ -7,6 +7,7 @@ import com.algolia.search.http.AlgoliaRequestKind;
 import com.algolia.search.http.AsyncAlgoliaHttpClient;
 import com.algolia.search.http.HttpMethod;
 import com.algolia.search.inputs.*;
+import com.algolia.search.inputs.analytics.ABTest;
 import com.algolia.search.inputs.batch.*;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
 import com.algolia.search.inputs.query_rules.Rule;
@@ -87,6 +88,10 @@ public class AsyncAPIClient {
    */
   public <T> AsyncIndex<T> initIndex(@Nonnull String name, @Nonnull Class<T> klass) {
     return new AsyncIndex<>(name, klass, this);
+  }
+
+  public AsyncAnalytics initAnalytics() {
+    return new AsyncAnalytics(this);
   }
 
   /**
@@ -1296,5 +1301,60 @@ public class AsyncAPIClient {
                     AsyncTask.class)
                 .setData(query))
         .thenApply(s -> s.setIndex(indexName));
+  }
+
+  CompletableFuture<AsyncTaskABTest> AddABTest(ABTest abtest) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.POST,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                AsyncTaskABTest.class)
+            .setData(abtest));
+  }
+
+  CompletableFuture<AsyncTaskABTest> StopABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.POST,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id), "stop"),
+            RequestOptions.empty,
+            AsyncTaskABTest.class));
+  }
+
+  CompletableFuture<AsyncTaskABTest> DeleteABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.DELETE,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            AsyncTaskABTest.class));
+  }
+
+  CompletableFuture<ABTest> GetABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.GET,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            ABTest.class));
+  }
+
+  CompletableFuture<ABTests> GetABTests(int offset, int limit) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.GET,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                ABTests.class)
+            .setParameters(
+                ImmutableMap.of(
+                    "offset", Integer.toString(offset),
+                    "limit", Integer.toString(limit))));
   }
 }

algoliasearch-common/src/main/java/com/algolia/search/AsyncAnalytics.java
@@ -1,0 +1,41 @@
+package com.algolia.search;
+
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.inputs.analytics.ABTest;
+import com.algolia.search.objects.tasks.async.AsyncTaskABTest;
+import com.algolia.search.responses.ABTests;
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncAnalytics implements Serializable {
+
+  private AsyncAPIClient client;
+
+  AsyncAnalytics(AsyncAPIClient client) {
+    this.client = client;
+  }
+
+  public CompletableFuture<AsyncTaskABTest> AddABTest(ABTest abtest) {
+    return client.AddABTest(abtest);
+  }
+
+  public CompletableFuture<AsyncTaskABTest> StopABTest(long id) {
+    return client.StopABTest(id);
+  }
+
+  public CompletableFuture<AsyncTaskABTest> DeleteABTest(long id) {
+    return client.DeleteABTest(id);
+  }
+
+  public CompletableFuture<ABTest> GetABTest(long id) {
+    return client.GetABTest(id);
+  }
+
+  public CompletableFuture<ABTests> GetABTests(int offset, int limit) {
+    return client.GetABTests(offset, limit);
+  }
+
+  public void WaitTask(AsyncTaskABTest task) throws AlgoliaException {
+    this.client.waitTask(task);
+  }
+}

algolia/algoliasearch-client-java

algoliasearch-common/src/main/java/com/algolia/search/APIClient.java
@@ -7,6 +7,7 @@ import com.algolia.search.http.AlgoliaRequest;
 import com.algolia.search.http.AlgoliaRequestKind;
 import com.algolia.search.http.HttpMethod;
 import com.algolia.search.inputs.*;
+import com.algolia.search.inputs.analytics.ABTest;
 import com.algolia.search.inputs.batch.*;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
 import com.algolia.search.inputs.query_rules.Rule;
@@ -1377,6 +1378,61 @@ public class APIClient {
                 .setData(queryRules));
 
     return task.setAPIClient(this).setIndex(indexName);
+  }
+
+  public TaskABTest addABTest(ABTest abtest) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.POST,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                TaskABTest.class)
+            .setData(abtest));
+  }
+
+  public TaskABTest stopABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.POST,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id), "stop"),
+            RequestOptions.empty,
+            TaskABTest.class));
+  }
+
+  public TaskABTest deleteABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.DELETE,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            TaskABTest.class));
+  }
+
+  public ABTest getABTest(long id) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.GET,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            ABTest.class));
+  }
+
+  public ABTests getABTests(int offset, int limit) throws AlgoliaException {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.GET,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                ABTests.class)
+            .setParameters(
+                ImmutableMap.of(
+                    "offset", Integer.toString(offset),
+                    "limit", Integer.toString(limit))));
   }
 
   /** Used internally for deleteByQuery */

algoliasearch-common/src/main/java/com/algolia/search/Analytics.java
@@ -1,10 +1,40 @@
 package com.algolia.search;
 
-public class Analytics {
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.inputs.analytics.ABTest;
+import com.algolia.search.objects.tasks.sync.TaskABTest;
+import com.algolia.search.responses.ABTests;
+import java.io.Serializable;
+
+public class Analytics implements Serializable {
 
   private APIClient client;
 
   Analytics(APIClient client) {
     this.client = client;
+  }
+
+  public TaskABTest addABTest(ABTest abtest) throws AlgoliaException {
+    return client.addABTest(abtest);
+  }
+
+  public TaskABTest stopABTest(long id) throws AlgoliaException {
+    return client.stopABTest(id);
+  }
+
+  public TaskABTest deleteABTest(long id) throws AlgoliaException {
+    return client.deleteABTest(id);
+  }
+
+  public ABTest getABTest(long id) throws AlgoliaException {
+    return client.getABTest(id);
+  }
+
+  public ABTests getABTests(int offset, int limit) throws AlgoliaException {
+    return client.getABTests(offset, limit);
+  }
+
+  public void waitTask(TaskABTest task) throws AlgoliaException {
+    this.client.waitTask(task);
   }
 }

algoliasearch-common/src/main/java/com/algolia/search/AsyncAPIClient.java
@@ -7,6 +7,7 @@ import com.algolia.search.http.AlgoliaRequestKind;
 import com.algolia.search.http.AsyncAlgoliaHttpClient;
 import com.algolia.search.http.HttpMethod;
 import com.algolia.search.inputs.*;
+import com.algolia.search.inputs.analytics.ABTest;
 import com.algolia.search.inputs.batch.*;
 import com.algolia.search.inputs.partial_update.PartialUpdateOperation;
 import com.algolia.search.inputs.query_rules.Rule;
@@ -87,6 +88,10 @@ public class AsyncAPIClient {
    */
   public <T> AsyncIndex<T> initIndex(@Nonnull String name, @Nonnull Class<T> klass) {
     return new AsyncIndex<>(name, klass, this);
+  }
+
+  public AsyncAnalytics initAnalytics() {
+    return new AsyncAnalytics(this);
   }
 
   /**
@@ -1296,5 +1301,60 @@ public class AsyncAPIClient {
                     AsyncTask.class)
                 .setData(query))
         .thenApply(s -> s.setIndex(indexName));
+  }
+
+  CompletableFuture<AsyncTaskABTest> addABTest(ABTest abtest) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.POST,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                AsyncTaskABTest.class)
+            .setData(abtest));
+  }
+
+  CompletableFuture<AsyncTaskABTest> stopABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.POST,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id), "stop"),
+            RequestOptions.empty,
+            AsyncTaskABTest.class));
+  }
+
+  CompletableFuture<AsyncTaskABTest> deleteABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.DELETE,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            AsyncTaskABTest.class));
+  }
+
+  CompletableFuture<ABTest> getABTest(long id) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+            HttpMethod.GET,
+            AlgoliaRequestKind.ANALYTICS_API,
+            Arrays.asList("2", "abtests", Long.toString(id)),
+            RequestOptions.empty,
+            ABTest.class));
+  }
+
+  CompletableFuture<ABTests> getABTests(int offset, int limit) {
+    return httpClient.requestAnalytics(
+        new AlgoliaRequest<>(
+                HttpMethod.GET,
+                AlgoliaRequestKind.ANALYTICS_API,
+                Arrays.asList("2", "abtests"),
+                RequestOptions.empty,
+                ABTests.class)
+            .setParameters(
+                ImmutableMap.of(
+                    "offset", Integer.toString(offset),
+                    "limit", Integer.toString(limit))));
   }
 }

algoliasearch-common/src/main/java/com/algolia/search/AsyncAnalytics.java
@@ -1,0 +1,41 @@
+package com.algolia.search;
+
+import com.algolia.search.exceptions.AlgoliaException;
+import com.algolia.search.inputs.analytics.ABTest;
+import com.algolia.search.objects.tasks.async.AsyncTaskABTest;
+import com.algolia.search.responses.ABTests;
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncAnalytics implements Serializable {
+
+  private AsyncAPIClient client;
+
+  AsyncAnalytics(AsyncAPIClient client) {
+    this.client = client;
+  }
+
+  public CompletableFuture<AsyncTaskABTest> addABTest(ABTest abtest) {
+    return client.addABTest(abtest);
+  }
+
+  public CompletableFuture<AsyncTaskABTest> stopABTest(long id) {
+    return client.stopABTest(id);
+  }
+
+  public CompletableFuture<AsyncTaskABTest> deleteABTest(long id) {
+    return client.deleteABTest(id);
+  }
+
+  public CompletableFuture<ABTest> getABTest(long id) {
+    return client.getABTest(id);
+  }
+
+  public CompletableFuture<ABTests> getABTests(int offset, int limit) {
+    return client.getABTests(offset, limit);
+  }
+
+  public void waitTask(AsyncTaskABTest task) throws AlgoliaException {
+    this.client.waitTask(task);
+  }
+}