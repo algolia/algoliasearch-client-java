ce989856983d650955f1523779d9e7008cde4ffc
Antoine Hémery <antoine.hemery@algolia.com>
2018-12-16 20:48:33 +0100

    Added: E2E tests for facet filters

--- algoliasearch-tests/src/test/java/com/algolia/search/integration/common/async/AsyncSearchTest.java
+++ algoliasearch-tests/src/test/java/com/algolia/search/integration/common/async/AsyncSearchTest.java
@@ -2,11 +2,9 @@ package com.algolia.search.integration.common.async;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.algolia.search.AlgoliaObject;
-import com.algolia.search.AlgoliaObjectForFaceting;
-import com.algolia.search.AsyncAlgoliaIntegrationTest;
-import com.algolia.search.AsyncIndex;
+import com.algolia.search.*;
 import com.algolia.search.exceptions.AlgoliaIndexNotFoundException;
+import com.algolia.search.objects.FacetFilters;
 import com.algolia.search.objects.IndexQuery;
 import com.algolia.search.objects.IndexSettings;
 import com.algolia.search.objects.Query;
@@ -15,6 +13,7 @@ import com.algolia.search.responses.SearchFacetResult;
 import com.algolia.search.responses.SearchResult;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import org.junit.Test;
 
@@ -57,6 +56,48 @@ public abstract class AsyncSearchTest extends AsyncAlgoliaIntegrationTest {
             .get();
 
     assertThat(search.getResults()).hasSize(2);
+  }
+
+  @Test
+  public void multiQueryWithFacetFilters() throws Exception {
+    AsyncIndex<AlgoliaObject> index = createIndex(AlgoliaObject.class);
+
+    waitForCompletion(
+        index.addObjects(
+            Arrays.asList(
+                new AlgoliaObject("algolia", 1),
+                new AlgoliaObject("algolia", 2),
+                new AlgoliaObject("algolia", 3))));
+
+    waitForCompletion(
+        index.setSettings(
+            new IndexSettings()
+                .setAttributesForFaceting(Arrays.asList("searchable(name)", "searchable(age)"))));
+
+    List<IndexQuery> queryFacetListOfString =
+        Arrays.asList(
+            new IndexQuery(
+                index,
+                new Query("").setFacetFilters(FacetFilters.ofList(Arrays.asList("name:algolia")))));
+
+    List<IndexQuery> queryFacetListListOfString =
+        Arrays.asList(
+            new IndexQuery(
+                index,
+                new Query("")
+                    .setFacetFilters(
+                        FacetFilters.ofListOfList(
+                            Arrays.asList(
+                                Arrays.asList("name:algolia"), Arrays.asList("age:3"))))));
+
+    MultiQueriesResult searchWithFacetListOfString =
+        client.multipleQueries(queryFacetListOfString).get();
+    MultiQueriesResult searchWithFacetListOfListOfString =
+        client.multipleQueries(queryFacetListListOfString).get();
+
+    assertThat(searchWithFacetListOfString.getResults().get(0).getParams()).contains("name");
+    assertThat(searchWithFacetListOfListOfString.getResults().get(0).getParams()).contains("name");
+    assertThat(searchWithFacetListOfListOfString.getResults().get(0).getParams()).contains("age");
   }
 
   @Test
