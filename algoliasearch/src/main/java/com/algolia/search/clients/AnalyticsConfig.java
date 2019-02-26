2efeec28b0b25c089a26f97f25c50f8ba8a09b11
Antoine Hémery <antoine.hemery@algolia.com>
2019-02-26 14:07:04 +0100

    Scaffolding: AnalyticsClient

---
+++ algoliasearch/src/main/java/com/algolia/search/clients/AnalyticsConfig.java
@@ -0,0 +1,26 @@
+package com.algolia.search.clients;
+
+import com.algolia.search.models.CallType;
+import com.algolia.search.transport.StatefulHost;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
+public class AnalyticsConfig extends AlgoliaConfig {
+
+  public AnalyticsConfig(String applicationID, String apiKey) {
+    super(applicationID, apiKey);
+
+    List<StatefulHost> hosts =
+            Collections.singletonList(
+                    new StatefulHost(
+                            "analytics.algolia.com",
+                            true,
+                            LocalDate.now(ZoneOffset.UTC),
+                            EnumSet.of(CallType.READ, CallType.WRITE)));
+
+    this.setDefaultHosts(hosts);
+  }
+}
