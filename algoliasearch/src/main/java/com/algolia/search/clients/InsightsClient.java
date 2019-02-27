698a89e89acd97964032c6a1dfd886c1171326a0
Antoine Hémery <antoine.hemery@algolia.com>
2019-02-27 11:33:53 +0100

    Added: InsightsClient async methods

--- algoliasearch/src/main/java/com/algolia/search/clients/InsightsClient.java
+++ algoliasearch/src/main/java/com/algolia/search/clients/InsightsClient.java
@@ -2,10 +2,20 @@ package com.algolia.search.clients;
 
 import com.algolia.search.http.AlgoliaHttpRequester;
 import com.algolia.search.http.IHttpRequester;
+import com.algolia.search.inputs.insights.InsightsEvent;
+import com.algolia.search.inputs.insights.InsightsRequest;
+import com.algolia.search.inputs.insights.InsightsResult;
+import com.algolia.search.models.CallType;
+import com.algolia.search.models.HttpMethod;
+import com.algolia.search.objects.RequestOptions;
 import com.algolia.search.transport.HttpTransport;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import javax.annotation.Nonnull;
 
+@SuppressWarnings("WeakerAccess")
 public class InsightsClient {
 
   private final HttpTransport transport;
@@ -34,5 +44,59 @@ public class InsightsClient {
 
     this.config = config;
     this.transport = new HttpTransport(config, httpRequester);
+  }
+
+  /** @param userToken the user config */
+  public UserInsightsClient user(@Nonnull String userToken) {
+    return new UserInsightsClient(userToken, this);
+  }
+
+  /**
+   * This command pushes an event to the Insights API.
+   *
+   * @param event An event
+   */
+  public CompletableFuture<InsightsResult> sendEventsAsync(@Nonnull InsightsEvent event) {
+    List<InsightsEvent> events = Collections.singletonList(event);
+    return sendEventsAsync(events, new RequestOptions());
+  }
+
+  /**
+   * This command pushes an event to the Insights API.
+   *
+   * @param event An event
+   * @param requestOptions RequestOptions
+   */
+  public CompletableFuture<InsightsResult> sendEventsAsync(
+      @Nonnull InsightsEvent event, @Nonnull RequestOptions requestOptions) {
+    List<InsightsEvent> events = Collections.singletonList(event);
+    return sendEventsAsync(events, requestOptions);
+  }
+
+  /**
+   * This command pushes an array of events to the Insights API.
+   *
+   * @param events List of events
+   */
+  public CompletableFuture<InsightsResult> sendEventsAsync(@Nonnull List<InsightsEvent> events) {
+    return sendEventsAsync(events, new RequestOptions());
+  }
+
+  /**
+   * This command pushes an array of events to the Insights API.
+   *
+   * @param events List of events
+   * @param requestOptions RequestOptions
+   */
+  public CompletableFuture<InsightsResult> sendEventsAsync(
+      @Nonnull List<InsightsEvent> events, @Nonnull RequestOptions requestOptions) {
+    InsightsRequest request = new InsightsRequest().setEvents(events);
+    return transport.executeRequestAsync(
+        HttpMethod.POST,
+        "/1/events",
+        CallType.WRITE,
+        request,
+        InsightsResult.class,
+        requestOptions);
   }
 }
