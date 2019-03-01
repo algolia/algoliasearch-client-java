027d30fbb909bb92c0d841465ba8322171cb7e4f
Antoine Hémery <antoine.hemery@algolia.com>
2019-03-01 17:57:32 +0100

    Added: splitIntoBatches

--- algoliasearch/src/main/java/com/algolia/search/models/BatchRequest.java
+++ algoliasearch/src/main/java/com/algolia/search/models/BatchRequest.java
@@ -2,16 +2,29 @@ package com.algolia.search.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @SuppressWarnings("unused")
-public class BatchRequest implements Serializable {
+public class BatchRequest<T> implements Serializable {
 
   private List<BatchOperation> requests;
 
   public BatchRequest(List<BatchOperation> requests) {
     this.requests = requests;
+  }
+
+  public BatchRequest(String actionType, Iterable<T> data) {
+    Objects.requireNonNull(actionType, "Action type is required.");
+    Objects.requireNonNull(data, "Data are required.");
+
+    this.requests = new ArrayList<>();
+
+    for (T item : data) {
+      this.requests.add(new BatchOperation<>(actionType, item));
+    }
   }
 
   public List<BatchOperation> getRequests() {
