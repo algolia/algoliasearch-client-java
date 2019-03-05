59ace7d9e42c58fc699b7663a40b22d0f203f731
Antoine Hémery <antoine.hemery@algolia.com>
2019-03-05 17:14:03 +0100

    Added: rules and synonyms methods
    - saveRule,saveRules,replaceAllRules
    - searchSynonym,getSynoym,saveSynonym(s),replaceAllSynonyms

--- algoliasearch-common/src/main/java/com/algolia/search/objects/SynonymQuery.java
+++ algoliasearch-common/src/main/java/com/algolia/search/objects/SynonymQuery.java
@@ -1,12 +1,9 @@
 package com.algolia.search.objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSetter;
-import com.google.common.base.Joiner;
 import java.io.Serializable;
-import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -15,7 +12,7 @@ import java.util.List;
 public class SynonymQuery implements Serializable {
 
   private String query;
-  private String type;
+  private List<String> type;
   private Long page;
   private Long hitsPerPage;
 
@@ -32,34 +29,11 @@ public class SynonymQuery implements Serializable {
     return this;
   }
 
-  /**
-   * use {@link #getType()}
-   *
-   * @return
-   */
-  @Deprecated
-  @JsonIgnore
-  public List<String> getTypes() {
-    return Collections.singletonList(type);
-  }
-
-  /**
-   * use {@link #setType(String)}
-   *
-   * @return
-   */
-  @Deprecated
-  @JsonIgnore
-  public SynonymQuery setTypes(List<String> types) {
-    this.type = Joiner.on(",").join(types);
-    return this;
-  }
-
-  public String getType() {
+  public List<String> getType() {
     return type;
   }
 
-  public SynonymQuery setType(String type) {
+  public SynonymQuery setType(List<String> type) {
     this.type = type;
     return this;
   }
