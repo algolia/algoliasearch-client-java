652348d106e3d7400f2b23bee4ee79b40880f02f
Antoine Hémery <antoine.hemery@algolia.com>
2019-03-19 09:59:57 +0100

    Cleanup: exceptions

--- algoliasearch/src/main/java/com/algolia/search/exceptions/AlgoliaRetryException.java
+++ algoliasearch/src/main/java/com/algolia/search/exceptions/AlgoliaRetryException.java
@@ -2,8 +2,6 @@ package com.algolia.search.exceptions;
 
 public class AlgoliaRetryException extends AlgoliaRuntimeException {
 
-  private static final long serialVersionUID = 815496271516822851L;
-
   public AlgoliaRetryException(String message, Throwable cause) {
     super(message, cause);
   }
