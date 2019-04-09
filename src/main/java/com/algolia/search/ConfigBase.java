d555faaecf21f16c586216697d6de77979d3d58d
Antoine Hémery <antoine.hemery@gmail.com>
2019-04-09 15:11:27 +0200

    Added: Configuration Builder (#549)
    * Added: Configuration Builder
    
    * Updated: protection level of ConfigBase
    
    * Removed: customHosts constructor in builders
    - Also renamed setCustomHosts to setHosts
    
    * Removed: customHosts and defaultHosts in favor of hosts
    - Added constructor with hosts in ConfigBase
    - Calling super in child classes with buildHosts() method
    
    * Removed: unused parameters in Analytics/Insights createDefaultHosts()
    
    * Removed: unused ApiKeys parameter in searchConfig/CreateDefault hosts

--- src/main/java/com/algolia/search/ConfigBase.java
+++ src/main/java/com/algolia/search/ConfigBase.java
@@ -1,44 +1,133 @@
 package com.algolia.search;
 
+import com.algolia.search.util.AlgoliaUtils;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
+import javax.annotation.Nonnull;
 
-/** * Algolia's clients common configuration */
+/** Algolia's clients common configuration */
 @SuppressWarnings("WeakerAccess")
-public abstract class ConfigBase {
+abstract class ConfigBase {
 
-  /**
-   * Creates an default configuration with the forkJoinPool.
-   *
-   * @param applicationID The Application ID
-   * @param apiKey The API Key.
-   */
-  ConfigBase(String applicationID, String apiKey) {
-    this(applicationID, apiKey, ForkJoinPool.commonPool());
+  private static final String javaVersion = System.getProperty("java.version");
+  private static final String clientVersion = "3.0.0.0";
+  private final String applicationID;
+  private final String apiKey;
+  private final HashMap<String, String> defaultHeaders;
+  private final int batchSize;
+  private final Integer readTimeOut;
+  private final Integer writeTimeOut;
+  private final Integer connectTimeOut;
+  private final List<StatefulHost> hosts;
+  private final ExecutorService executor;
+
+  /** Config base builder to ensure the immutability of the configuration. */
+  public abstract static class Builder<T extends Builder<T>> {
+
+    private final String applicationID;
+    private final String apiKey;
+    private final HashMap<String, String> defaultHeaders;
+    private int batchSize;
+    private Integer readTimeOut;
+    private Integer writeTimeOut;
+    private Integer connectTimeOut;
+    private List<StatefulHost> hosts;
+    private final ExecutorService executor;
+
+    /**
+     * Builds a base configuration
+     *
+     * @param applicationID The Algolia Application ID
+     * @param apiKey The API Key: could be Admin API Key or Search API Key
+     * @throws NullPointerException if ApplicationID/ApiKey/Config/Requester is null
+     */
+    public Builder(
+        @Nonnull String applicationID,
+        @Nonnull String apiKey,
+        @Nonnull List<StatefulHost> defaultHosts) {
+      this.applicationID = applicationID;
+      this.apiKey = apiKey;
+      this.batchSize = 1000;
+      this.hosts = defaultHosts;
+      this.defaultHeaders = new HashMap<>();
+      this.defaultHeaders.put(Defaults.ALGOLIA_APPLICATION_HEADER, applicationID);
+      this.defaultHeaders.put(Defaults.ALGOLIA_KEY_HEADER, apiKey);
+      this.defaultHeaders.put(
+          Defaults.USER_AGENT_HEADER,
+          String.format("Algolia for Java (%s); JVM (%s)", clientVersion, javaVersion));
+      this.defaultHeaders.put(Defaults.ACCEPT_HEADER, Defaults.APPLICATION_JSON);
+      this.defaultHeaders.put(Defaults.ACCEPT_ENCODING_HEADER, Defaults.CONTENT_ENCODING_GZIP);
+      this.executor = ForkJoinPool.commonPool();
+    }
+
+    /** To prevent unchecked cast warning. */
+    public abstract T getThis();
+
+    /** Overrides the default batch size for save methods. Default = 1000 objects per chunk. */
+    public T setBatchSize(int batchSize) {
+      this.batchSize = batchSize;
+      return getThis();
+    }
+
+    /** Overrides the default read timeout. Default = 1000ms In milliseconds */
+    public T setReadTimeOut(Integer readTimeOut) {
+      this.readTimeOut = readTimeOut;
+      return getThis();
+    }
+
+    /** Overrides the default write timeout. Default = 30000ms In milliseconds */
+    public T setWriteTimeOut(Integer writeTimeOut) {
+      this.writeTimeOut = writeTimeOut;
+      return getThis();
+    }
+
+    /** Overrides the default connect timeout. Default = 2000ms In milliseconds */
+    public T setConnectTimeOut(Integer connectTimeOut) {
+      this.connectTimeOut = connectTimeOut;
+      return getThis();
+    }
+
+    /** Sets a list of specific host to target. Default hosts will be overridden. */
+    public T setHosts(@Nonnull List<StatefulHost> customHosts) {
+      this.hosts = customHosts;
+      return getThis();
+    }
   }
 
-  /**
-   * Creates a advanced configuration with a custom ExecutorService.
-   *
-   * @param applicationID The Application ID
-   * @param apiKey The API Key
-   * @param executor The Executor service
-   */
-  ConfigBase(String applicationID, String apiKey, ExecutorService executor) {
-    this.applicationID = applicationID;
-    this.apiKey = apiKey;
-    this.executor = executor;
-    this.batchSize = 1000;
-    this.defaultHeaders = new HashMap<>();
-    this.defaultHeaders.put(Defaults.ALGOLIA_APPLICATION_HEADER, getApplicationID());
-    this.defaultHeaders.put(Defaults.ALGOLIA_KEY_HEADER, getApiKey());
-    this.defaultHeaders.put(
-        Defaults.USER_AGENT_HEADER,
-        String.format("Algolia for Java (%s); JVM (%s)", clientVersion, javaVersion));
-    this.defaultHeaders.put(Defaults.ACCEPT_HEADER, Defaults.APPLICATION_JSON);
-    this.defaultHeaders.put(Defaults.ACCEPT_ENCODING_HEADER, Defaults.CONTENT_ENCODING_GZIP);
+  protected ConfigBase(Builder<?> builder) {
+
+    Objects.requireNonNull(builder.applicationID, "An ApplicationID is required.");
+    Objects.requireNonNull(builder.apiKey, "An API key is required.");
+    Objects.requireNonNull(builder.hosts, "Default hosts are required.");
+
+    if (AlgoliaUtils.isEmptyWhiteSpace(builder.applicationID)) {
+      throw new NullPointerException("The ApplicationID can't be empty.");
+    }
+
+    if (AlgoliaUtils.isEmptyWhiteSpace(builder.apiKey)) {
+      throw new NullPointerException("The APIKey can't be empty.");
+    }
+
+    this.apiKey = builder.apiKey;
+    this.applicationID = builder.applicationID;
+    this.defaultHeaders = builder.defaultHeaders;
+    this.batchSize = builder.batchSize;
+    this.readTimeOut = builder.readTimeOut;
+    this.writeTimeOut = builder.writeTimeOut;
+    this.connectTimeOut = builder.connectTimeOut;
+    this.hosts = builder.hosts;
+    this.executor = builder.executor;
+  }
+
+  public static String getJavaVersion() {
+    return javaVersion;
+  }
+
+  public static String getClientVersion() {
+    return clientVersion;
   }
 
   public String getApplicationID() {
@@ -57,74 +146,23 @@ public abstract class ConfigBase {
     return batchSize;
   }
 
-  /** Overrides the default batch size for save methods. Default = 1000 objects per chunk. */
-  public ConfigBase setBatchSize(int batchSize) {
-    this.batchSize = batchSize;
-    return this;
-  }
-
   public Integer getReadTimeOut() {
     return readTimeOut;
-  }
-
-  /** Overrides the default read timeout. Default = 1000ms In milliseconds */
-  public ConfigBase setReadTimeOut(Integer readTimeOut) {
-    this.readTimeOut = readTimeOut;
-    return this;
   }
 
   public Integer getWriteTimeOut() {
     return writeTimeOut;
   }
 
-  /** Overrides the default write timeout. Default = 30000ms In milliseconds */
-  public ConfigBase setWriteTimeOut(Integer writeTimeOut) {
-    this.writeTimeOut = writeTimeOut;
-    return this;
-  }
-
   public Integer getConnectTimeOut() {
     return connectTimeOut;
   }
 
-  /** Overrides the default connect timeout. Default = 2000ms In milliseconds */
-  public ConfigBase setConnectTimeOut(Integer connectTimeOut) {
-    this.connectTimeOut = connectTimeOut;
-    return this;
-  }
-
-  public List<StatefulHost> getDefaultHosts() {
-    return defaultHosts;
-  }
-
-  void setDefaultHosts(List<StatefulHost> defaultHosts) {
-    this.defaultHosts = defaultHosts;
-  }
-
-  public List<StatefulHost> getCustomHosts() {
-    return customHosts;
-  }
-
-  /** Sets a list of specific host to target. Default hosts will be overridden. */
-  public ConfigBase setCustomHosts(List<StatefulHost> customHosts) {
-    this.customHosts = customHosts;
-    return this;
+  public List<StatefulHost> getHosts() {
+    return hosts;
   }
 
   public ExecutorService getExecutor() {
     return executor;
   }
-
-  private static final String javaVersion = System.getProperty("java.version");
-  private final String clientVersion = "3.0.0.0";
-  private final String applicationID;
-  private final String apiKey;
-  private final HashMap<String, String> defaultHeaders;
-  private int batchSize;
-  private Integer readTimeOut;
-  private Integer writeTimeOut;
-  private Integer connectTimeOut;
-  private List<StatefulHost> defaultHosts;
-  private List<StatefulHost> customHosts;
-  private final ExecutorService executor;
 }
--- src/main/java/com/algolia/search/InsightsConfig.java
+++ src/main/java/com/algolia/search/InsightsConfig.java
@@ -1,56 +1,60 @@
 package com.algolia.search;
 
 import com.algolia.search.models.common.CallType;
+import com.algolia.search.util.AlgoliaUtils;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import javax.annotation.Nonnull;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
+@SuppressWarnings({"unused"})
 public final class InsightsConfig extends ConfigBase {
 
-  /**
-   * Creates an {@link InsightsConfig} with the default region ("us").
-   *
-   * @param applicationID The ApplicationID
-   * @param apiKey The API Key
-   */
-  public InsightsConfig(String applicationID, String apiKey) {
-    this(applicationID, apiKey, "us");
+  public static class Builder extends ConfigBase.Builder<Builder> {
+
+    /**
+     * Creates an {@link InsightsConfig} with the default region ("us").
+     *
+     * @param applicationID The ApplicationID
+     * @param apiKey The API Key
+     */
+    public Builder(@Nonnull String applicationID, @Nonnull String apiKey) {
+      this(applicationID, apiKey, "us");
+    }
+
+    /**
+     * Creates an {@link InsightsConfig} with a custom region.
+     *
+     * @param applicationID The ApplicationID
+     * @param apiKey The API Key
+     */
+    public Builder(@Nonnull String applicationID, @Nonnull String apiKey, @Nonnull String region) {
+      super(applicationID, apiKey, createDefaultHosts(region));
+    }
+
+    @Override
+    public Builder getThis() {
+      return this;
+    }
+
+    public InsightsConfig build() {
+      return new InsightsConfig(this);
+    }
+
+    /** Create default hosts for the insights configuration */
+    private static List<StatefulHost> createDefaultHosts(@Nonnull String region) {
+
+      if (AlgoliaUtils.isEmptyWhiteSpace(region)) {
+        throw new NullPointerException("The region can't be empty.");
+      }
+
+      return Collections.singletonList(
+          new StatefulHost(
+              "insights." + region + ".algolia.io", EnumSet.of(CallType.READ, CallType.WRITE)));
+    }
   }
 
-  /**
-   * Creates an Insight client configuration with a custom region.
-   *
-   * @param applicationID The ApplicationID
-   * @param apiKey The API Key
-   * @param region Region of the server. For example "us"
-   */
-  public InsightsConfig(String applicationID, String apiKey, String region) {
-    super(applicationID, apiKey);
-
-    List<StatefulHost> hosts =
-        Collections.singletonList(
-            new StatefulHost(
-                "insights." + region + ".algolia.io", EnumSet.of(CallType.READ, CallType.WRITE)));
-
-    this.setDefaultHosts(hosts);
-  }
-
-  /**
-   * Creates an {@link InsightsConfig} for the search client with custom {@link StatefulHost}.
-   * Warning: Defaults hosts are not set when setting custom {@link StatefulHost}.
-   *
-   * @param applicationID The ApplicationID
-   * @param apiKey The API Key
-   * @param customHosts List of custom hosts
-   */
-  public InsightsConfig(
-      @Nonnull String applicationID,
-      @Nonnull String apiKey,
-      @Nonnull List<StatefulHost> customHosts) {
-    super(applicationID, apiKey);
-
-    this.setCustomHosts(customHosts);
+  private InsightsConfig(Builder builder) {
+    super(builder);
   }
 }
