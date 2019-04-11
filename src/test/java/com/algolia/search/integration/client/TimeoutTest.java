bae93fcaffe75e1ff455e3fbee1a997d7e2acd20
Benoit Perrot <benoit.perrot@algolia.com>
2019-04-11 18:06:22 +0200

    TimeoutTest: Split into "core" and "apache"
    Allowing dependency injection.

--- src/test/java/com/algolia/search/integration/client/TimeoutTest.java
+++ src/test/java/com/algolia/search/integration/client/TimeoutTest.java
@@ -1,7 +1,5 @@
 package com.algolia.search.integration.client;
 
-import static com.algolia.search.integration.IntegrationTestExtension.ALGOLIA_API_KEY_1;
-import static com.algolia.search.integration.IntegrationTestExtension.ALGOLIA_APPLICATION_ID_1;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import com.algolia.search.SearchClient;
@@ -10,16 +8,16 @@ import com.algolia.search.exceptions.AlgoliaRetryException;
 import java.io.IOException;
 import org.junit.jupiter.api.Disabled;
 
-class TimeoutTest {
+public abstract class TimeoutTest {
+
+  protected abstract SearchConfig.Builder createBuilder();
+
+  protected abstract SearchClient createClient(SearchConfig config);
 
   @Disabled
   void testUnreachableHostExceptions() {
     try (SearchClient client =
-        new SearchClient(
-            new SearchConfig.Builder(ALGOLIA_APPLICATION_ID_1, ALGOLIA_API_KEY_1)
-                .setConnectTimeOut(2)
-                .setReadTimeOut(2)
-                .build())) {
+        createClient(createBuilder().setConnectTimeOut(2).setReadTimeOut(2).build())) {
       assertThatThrownBy(() -> client.getLogsAsync().get())
           .hasCauseInstanceOf(AlgoliaRetryException.class);
     } catch (IOException e) {
