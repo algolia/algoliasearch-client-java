106be57eb3fe8f059fff76ba7e5fc96e77e7becd
Benoit Perrot <benoit.perrot@algolia.com>
2019-04-11 16:52:39 +0200

    SecuredAPIKeyTest: Make variables of attributes
    Limiting their visibility scope to what is really necessary.

--- src/test/java/com/algolia/search/integration/client/SecuredAPIKeyTest.java
+++ src/test/java/com/algolia/search/integration/client/SecuredAPIKeyTest.java
@@ -10,64 +10,51 @@ import com.algolia.search.integration.models.AlgoliaObject;
 import com.algolia.search.models.apikeys.SecuredApiKeyRestriction;
 import com.algolia.search.models.indexing.BatchIndexingResponse;
 import com.algolia.search.models.indexing.Query;
-import java.io.IOException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({IntegrationTestExtension.class})
 class SecuredAPIKeyTest {
-
-  private static SearchClient clientWithRestriction;
-  private SearchIndex<AlgoliaObject> index1;
-  private static SearchIndex<AlgoliaObject> index2;
-  private String index1Name;
-  private String index2Name;
-
-  SecuredAPIKeyTest() {
-    index1Name = getTestIndexName("secured_api_keys");
-    index2Name = getTestIndexName("secured_api_keys_dev");
-    index1 = searchClient.initIndex(index1Name, AlgoliaObject.class);
-    index2 = searchClient2.initIndex(index2Name, AlgoliaObject.class);
-  }
-
-  @AfterAll
-  static void after() throws IOException {
-    clientWithRestriction.close();
-    index2.deleteAsync();
-  }
-
   @Test
   void testSecuredAPIKey() throws Exception {
-    CompletableFuture<BatchIndexingResponse> addOneFuture =
-        index1.saveObjectAsync(new AlgoliaObject().setObjectID("one"));
-    CompletableFuture<BatchIndexingResponse> addTwoFuture =
-        index2.saveObjectAsync(new AlgoliaObject().setObjectID("one"));
+    String index1Name = getTestIndexName("secured_api_keys");
+    String index2Name = getTestIndexName("secured_api_keys_dev");
+    SearchIndex<AlgoliaObject> index1 = searchClient.initIndex(index1Name, AlgoliaObject.class);
+    SearchIndex<AlgoliaObject> index2 = searchClient2.initIndex(index2Name, AlgoliaObject.class);
+    try {
+      CompletableFuture<BatchIndexingResponse> addOneFuture =
+          index1.saveObjectAsync(new AlgoliaObject().setObjectID("one"));
+      CompletableFuture<BatchIndexingResponse> addTwoFuture =
+          index2.saveObjectAsync(new AlgoliaObject().setObjectID("one"));
 
-    SecuredApiKeyRestriction restriction =
-        new SecuredApiKeyRestriction()
-            .setRestrictIndices(Collections.singletonList(index1Name))
-            .setValidUntil(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(10).toEpochSecond());
+      SecuredApiKeyRestriction restriction =
+          new SecuredApiKeyRestriction()
+              .setRestrictIndices(Collections.singletonList(index1Name))
+              .setValidUntil(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(10).toEpochSecond());
 
-    addOneFuture.get().waitTask();
-    addTwoFuture.get().waitTask();
+      addOneFuture.get().waitTask();
+      addTwoFuture.get().waitTask();
 
-    String key = searchClient.generateSecuredAPIKey(ALGOLIA_SEARCH_KEY_1, restriction);
+      String key = searchClient.generateSecuredAPIKey(ALGOLIA_SEARCH_KEY_1, restriction);
 
-    clientWithRestriction = new SearchClient(ALGOLIA_APPLICATION_ID_1, key);
+      try (SearchClient clientWithRestriction = new SearchClient(ALGOLIA_APPLICATION_ID_1, key)) {
 
-    SearchIndex<AlgoliaObject> index1WithoutRestriction =
-        clientWithRestriction.initIndex(index1Name, AlgoliaObject.class);
-    SearchIndex<AlgoliaObject> index2WithRestriction =
-        clientWithRestriction.initIndex(index2Name, AlgoliaObject.class);
+        SearchIndex<AlgoliaObject> index1WithoutRestriction =
+            clientWithRestriction.initIndex(index1Name, AlgoliaObject.class);
+        SearchIndex<AlgoliaObject> index2WithRestriction =
+            clientWithRestriction.initIndex(index2Name, AlgoliaObject.class);
 
-    index1WithoutRestriction.searchAsync(new Query()).get();
+        index1WithoutRestriction.searchAsync(new Query()).get();
 
-    assertThatThrownBy(() -> index2WithRestriction.search(new Query()))
-        .hasMessageContaining("Index not allowed with this API key");
+        assertThatThrownBy(() -> index2WithRestriction.search(new Query()))
+            .hasMessageContaining("Index not allowed with this API key");
+      }
+    } finally {
+      index2.deleteAsync();
+    }
   }
 }
