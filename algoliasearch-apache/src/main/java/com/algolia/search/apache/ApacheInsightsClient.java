50d05192efae75014f1a51cd0f19ec770a4ac204
Antoine Hémery <antoine.hemery@algolia.com>
2019-04-15 14:53:44 +0200

    Architecture: multi-modules project

---
+++ algoliasearch-apache/src/main/java/com/algolia/search/apache/ApacheInsightsClient.java
@@ -0,0 +1,38 @@
+package com.algolia.search.apache;
+
+import com.algolia.search.InsightsClient;
+import com.algolia.search.InsightsConfig;
+import javax.annotation.Nonnull;
+
+/**
+ * Algolia's REST insights client that wraps an instance of the transporter. which wraps the HTTP
+ * Client This client allows to build typed requests and read typed responses. Requests are made
+ * under the Algolia's retry-strategy. This client is intended to be reused and it's thread-safe.
+ *
+ * @see <a href="https://www.algolia.com/doc/rest-api/insights/">Algolia.com</a>
+ */
+@SuppressWarnings("WeakerAccess")
+public class ApacheInsightsClient {
+
+  /**
+   * Creates a {@link InsightsClient} with the given credentials
+   *
+   * @param applicationID The Algolia Application ID
+   * @param apiKey The Algolia API Key
+   * @throws NullPointerException if ApplicationID/ApiKey is null
+   */
+  public static InsightsClient create(@Nonnull String applicationID, @Nonnull String apiKey) {
+    return create(new InsightsConfig.Builder(applicationID, apiKey).build());
+  }
+
+  /**
+   * Creates a {@link InsightsClient} with the given {@link InsightsConfig}
+   *
+   * @param config The configuration allows you to advanced configuration of the clients such as
+   *     batch size or custom hosts.
+   * @throws NullPointerException if Config is null
+   */
+  public static InsightsClient create(@Nonnull InsightsConfig config) {
+    return new InsightsClient(config, new ApacheHttpRequester(config));
+  }
+}
---
+++ algoliasearch-core/src/main/java/com/algolia/search/AnalyticsClient.java
@@ -0,0 +1,392 @@
+package com.algolia.search;
+
+import com.algolia.search.exceptions.AlgoliaApiException;
+import com.algolia.search.exceptions.AlgoliaRetryException;
+import com.algolia.search.exceptions.AlgoliaRuntimeException;
+import com.algolia.search.exceptions.LaunderThrowable;
+import com.algolia.search.models.HttpMethod;
+import com.algolia.search.models.RequestOptions;
+import com.algolia.search.models.analytics.*;
+import com.algolia.search.models.common.CallType;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import javax.annotation.Nonnull;
+
+/**
+ * Algolia's REST analytics client that wraps an instance of the transporter {@link HttpTransport}
+ * which wraps the HTTP Client This client allows to build typed requests and read typed responses.
+ * Requests are made under the Algolia's retry-strategy. This client is intended to be reused and
+ * it's thread-safe.
+ *
+ * @see <a href="https://www.algolia.com/doc/rest-api/analytics/">Algolia.com</a>
+ */
+@SuppressWarnings("WeakerAccess")
+public final class AnalyticsClient implements Closeable {
+
+  /** The transport layer. Must be reused. */
+  private final HttpTransport transport;
+
+  /**
+   * Creates a {@link AnalyticsClient} with the given {@link AnalyticsConfig}
+   *
+   * @param config The configuration allows you to advanced configuration of the clients such as
+   *     batch size or custom hosts.
+   * @param httpRequester Another HTTP Client than the default one.
+   * @throws NullPointerException if ApplicationID/ApiKey/Config/Requester is null
+   */
+  public AnalyticsClient(@Nonnull AnalyticsConfig config, @Nonnull HttpRequester httpRequester) {
+
+    Objects.requireNonNull(httpRequester, "An httpRequester is required.");
+    Objects.requireNonNull(config, "A configuration is required.");
+
+    this.transport = new HttpTransport(config, httpRequester);
+  }
+
+  /**
+   * Close the underlying Http Client
+   *
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void close() throws IOException {
+    transport.close();
+  }
+
+  /** Get an A/B test information and results. */
+  public ABTests getABTests() {
+    return LaunderThrowable.await(getABTestsAsync(0, 10, null));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public ABTests getABTests(RequestOptions requestOptions) {
+    return LaunderThrowable.await(getABTestsAsync(0, 10, requestOptions));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param offset Position of the starting record. Used for paging. 0 is the first record.
+   * @param limit Number of records to return. +used for paging. Limit is the size of the page.
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public ABTests getABTests(int offset, int limit) {
+    return LaunderThrowable.await(getABTestsAsync(offset, limit, null));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param offset Position of the starting record. Used for paging. 0 is the first record.
+   * @param limit Number of records to return. +used for paging. Limit is the size of the page.
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public ABTests getABTests(int offset, int limit, RequestOptions requestOptions) {
+    return LaunderThrowable.await(getABTestsAsync(offset, limit, requestOptions));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTests> getABTestsAsync() {
+    return getABTestsAsync(0, 10, null);
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTests> getABTestsAsync(RequestOptions requestOptions) {
+    return getABTestsAsync(0, 10, requestOptions);
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param offset Position of the starting record. Used for paging. 0 is the first record.
+   * @param limit Number of records to return. +used for paging. Limit is the size of the page.
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTests> getABTestsAsync(int offset, int limit) {
+    return getABTestsAsync(0, 10, null);
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param offset Position of the starting record. Used for paging. 0 is the first record.
+   * @param limit Number of records to return. +used for paging. Limit is the size of the page.
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTests> getABTestsAsync(
+      int offset, int limit, RequestOptions requestOptions) {
+
+    if (requestOptions == null) {
+      requestOptions = new RequestOptions();
+    }
+
+    requestOptions.addExtraQueryParameters("offset", Integer.toString(offset));
+    requestOptions.addExtraQueryParameters("limit", Integer.toString(limit));
+
+    return transport.executeRequestAsync(
+        HttpMethod.GET, "/2/abtests", CallType.READ, null, ABTests.class, requestOptions);
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public ABTestResponse getABTest(long id) {
+    return LaunderThrowable.await(getABTestAsync(id, null));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public ABTestResponse getABTest(long id, RequestOptions requestOptions) {
+    return LaunderThrowable.await(getABTestAsync(id, requestOptions));
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTestResponse> getABTestAsync(long id) {
+    return getABTestAsync(id, null);
+  }
+
+  /**
+   * Get an A/B test information and results.
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<ABTestResponse> getABTestAsync(long id, RequestOptions requestOptions) {
+    return transport.executeRequestAsync(
+        HttpMethod.GET, "/2/abtests/" + id, CallType.READ, ABTestResponse.class, requestOptions);
+  }
+
+  /**
+   * Marks the A/B Test as stopped. At this point, the test is over and cannot be restarted. As a
+   * result, your application is back to normal: index A will perform as usual, receiving 100% of
+   * all search requests. Associated metadata and metrics are still stored
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public StopAbTestResponse stopABTest(long id) {
+    return LaunderThrowable.await(stopABTestAsync(id, null));
+  }
+
+  /**
+   * Marks the A/B Test as stopped. At this point, the test is over and cannot be restarted. As a
+   * result, your application is back to normal: index A will perform as usual, receiving 100% of
+   * all search requests. Associated metadata and metrics are still stored
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public StopAbTestResponse stopABTest(long id, RequestOptions requestOptions) {
+    return LaunderThrowable.await(stopABTestAsync(id, requestOptions));
+  }
+
+  /**
+   * Marks the A/B Test as stopped. At this point, the test is over and cannot be restarted. As a
+   * result, your application is back to normal: index A will perform as usual, receiving 100% of
+   * all search requests. Associated metadata and metrics are still stored
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<StopAbTestResponse> stopABTestAsync(long id) {
+    return stopABTestAsync(id, null);
+  }
+
+  /**
+   * Marks the A/B Test as stopped. At this point, the test is over and cannot be restarted. As a
+   * result, your application is back to normal: index A will perform as usual, receiving 100% of
+   * all search requests. Associated metadata and metrics are still stored
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<StopAbTestResponse> stopABTestAsync(
+      long id, RequestOptions requestOptions) {
+    return transport.executeRequestAsync(
+        HttpMethod.POST,
+        "/2/abtests/" + id + "/stop",
+        CallType.WRITE,
+        StopAbTestResponse.class,
+        requestOptions);
+  }
+
+  /**
+   * Deletes the A/B Test and removes all associated metadata and metrics.
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public DeleteAbTestResponse deleteABTest(long id) {
+    return LaunderThrowable.await(deleteABTestAsync(id, null));
+  }
+
+  /**
+   * Deletes the A/B Test and removes all associated metadata and metrics.
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public DeleteAbTestResponse deleteABTest(long id, RequestOptions requestOptions) {
+    return LaunderThrowable.await(deleteABTestAsync(id, requestOptions));
+  }
+
+  /**
+   * Deletes the A/B Test and removes all associated metadata and metrics.
+   *
+   * @param id The ABTest ID
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<DeleteAbTestResponse> deleteABTestAsync(long id) {
+    return deleteABTestAsync(id, null);
+  }
+
+  /**
+   * Deletes the A/B Test and removes all associated metadata and metrics.
+   *
+   * @param id The ABTest ID
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<DeleteAbTestResponse> deleteABTestAsync(
+      long id, RequestOptions requestOptions) {
+    return transport.executeRequestAsync(
+        HttpMethod.DELETE,
+        "/2/abtests/" + id,
+        CallType.WRITE,
+        DeleteAbTestResponse.class,
+        requestOptions);
+  }
+
+  /**
+   * Creates a new AB Test with provided configuration
+   *
+   * @param abTest The definition of the A/B test
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public AddABTestResponse addABTest(@Nonnull ABTest abTest) throws AlgoliaRuntimeException {
+    return LaunderThrowable.await(addABTestAsync(abTest, null));
+  }
+
+  /**
+   * Creates a new AB Test with provided configuration
+   *
+   * @param abTest The definition of the A/B test
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public AddABTestResponse addABTest(@Nonnull ABTest abTest, RequestOptions requestOptions)
+      throws AlgoliaRuntimeException {
+    return LaunderThrowable.await(addABTestAsync(abTest, requestOptions));
+  }
+
+  /**
+   * Creates a new AB Test with provided configuration
+   *
+   * @param abTest The definition of the A/B test
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<AddABTestResponse> addABTestAsync(@Nonnull ABTest abTest) {
+    return addABTestAsync(abTest, null);
+  }
+
+  /**
+   * Creates a new AB Test with provided configuration
+   *
+   * @param abTest The definition of the A/B test
+   * @param requestOptions Options to pass to this request
+   * @throws AlgoliaRetryException When the retry has failed on all hosts
+   * @throws AlgoliaApiException When the API sends an http error code
+   * @throws AlgoliaRuntimeException When an error occurred during the serialization
+   */
+  public CompletableFuture<AddABTestResponse> addABTestAsync(
+      @Nonnull ABTest abTest, RequestOptions requestOptions) {
+
+    Objects.requireNonNull(abTest, "An ABTest is required.");
+
+    return transport.executeRequestAsync(
+        HttpMethod.POST,
+        "/2/abtests",
+        CallType.WRITE,
+        abTest,
+        AddABTestResponse.class,
+        requestOptions);
+  }
+}
