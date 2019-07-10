37d16de770b9450bdf1d74801a7288c561eb1cac
Anthony Seure <anthony.seure@gmail.com>
2019-07-10 10:40:04 +0200

    fixed(synonym): handle deserialization of lowercase variant of synonym types
    Synonyms saved through the Algolia dashboard see their types stored as
    lowercase. Jackson not being able to deserialize lowercase enums and
    keys into their upper-case or mixed-case counterparts, retrieving
    synonyms that were saved from the dashboard made implementations crash.
    
    This commit adds new package-private interface to list all lowercase
    variants of the synonym types and use them as JsonSubTypes annotations
    of the AbstractSynonym class.
    
    Note that we considered using ACCEPT_CASE_INSENSITIVE_PROPERTIES
    MapperFeature from jackson-databind but this needs to be set globally at
    the ObjectMapper's level, which we do not want.
    
    Close #597

--- algoliasearch-common/src/main/java/com/algolia/search/inputs/synonym/SynonymType.java
+++ algoliasearch-common/src/main/java/com/algolia/search/inputs/synonym/SynonymType.java
@@ -1,10 +1,15 @@
 package com.algolia.search.inputs.synonym;
 
 interface SynonymType {
-
   String ALT_CORRECTION_1 = "altCorrection1";
   String ALT_CORRECTION_2 = "altCorrection2";
   String ONE_WAY_SYNONYM = "oneWaySynonym";
   String PLACEHOLDER = "placeholder";
   String SYNONYM = "synonym";
+}
+
+interface SynonymTypeLowerCase {
+  static String ALT_CORRECTION_1 = "altcorrection1";
+  static String ALT_CORRECTION_2 = "altcorrection2";
+  static String ONE_WAY_SYNONYM = "onewaysynonym";
 }
