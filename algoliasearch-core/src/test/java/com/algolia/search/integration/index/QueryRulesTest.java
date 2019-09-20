4cd679668e4e275faeb23fbfac5afa0c6d9a6207
Antoine Hémery <antoine.hemery@algolia.com>
2019-09-20 10:15:54 +0200

    internal: added contextual rules CTS test case
    fix #632

--- algoliasearch-core/src/test/java/com/algolia/search/integration/index/QueryRulesTest.java
+++ algoliasearch-core/src/test/java/com/algolia/search/integration/index/QueryRulesTest.java
@@ -7,6 +7,7 @@ import com.algolia.search.SearchClient;
 import com.algolia.search.SearchIndex;
 import com.algolia.search.iterators.RulesIterable;
 import com.algolia.search.models.indexing.BatchIndexingResponse;
+import com.algolia.search.models.indexing.Query;
 import com.algolia.search.models.indexing.SearchResult;
 import com.algolia.search.models.rules.*;
 import com.algolia.search.models.settings.IndexSettings;
@@ -19,7 +20,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
@@ -46,9 +46,9 @@ public abstract class QueryRulesTest {
     CompletableFuture<BatchIndexingResponse> saveObjectsFuture =
         index.saveObjectsAsync(objectsToSave);
 
-    // Set attributesForFaceting to [“brand”] using setSettings and collect the taskID
+    // Set attributesForFaceting to [“brand”, "model"] using setSettings and collect the taskID
     IndexSettings settings =
-        new IndexSettings().setAttributesForFaceting(Collections.singletonList("brand"));
+        new IndexSettings().setAttributesForFaceting(Arrays.asList("brand", "model"));
 
     CompletableFuture<SetSettingsResponse> settingsFuture = index.setSettingsAsync(settings);
 
@@ -103,10 +103,26 @@ public abstract class QueryRulesTest {
                     .setAlternatives(Alternatives.of(true)))
             .setConsequence(consequenceToBatch);
 
+    // Third rule to save
+    Consequence consequenceToBatch2 =
+        new Consequence().setParams(new ConsequenceParams().setFilters("brand:OnePlus"));
+
+    Rule ruleToSave3 = new Rule().setObjectID("query_promo").setConsequence(consequenceToBatch2);
+
+    // Fourth rule to save
+    Consequence consequenceToBatch3 =
+        new Consequence().setParams(new ConsequenceParams().setFilters("model:One"));
+
+    Rule ruleToSave4 =
+        new Rule()
+            .setObjectID("query_promo_only_summer")
+            .setConsequence(consequenceToBatch3)
+            .setCondition(new Condition().setContext("summer"));
+
     // Sending rules request
     CompletableFuture<SaveRuleResponse> saveRuleFuture = index.saveRuleAsync(ruleToSave);
     CompletableFuture<SaveRuleResponse> batchRuleFuture =
-        index.saveRulesAsync(Collections.singletonList(ruleToSave2));
+        index.saveRulesAsync(Arrays.asList(ruleToSave2, ruleToSave3, ruleToSave4));
 
     // Wait for the settings to be complete before saving
     saveObjectsFuture.get().waitTask();
@@ -114,18 +130,28 @@ public abstract class QueryRulesTest {
     saveRuleFuture.get().waitTask();
     batchRuleFuture.get().waitTask();
 
-    CompletableFuture<Rule> getRuleFuture = index.getRuleAsync(ruleToSave.getObjectID());
-    CompletableFuture<Rule> getBatchedRuleFuture = index.getRuleAsync(ruleToSave2.getObjectID());
+    CompletableFuture<Rule> rule1Future = index.getRuleAsync(ruleToSave.getObjectID());
+    CompletableFuture<Rule> rule2Future = index.getRuleAsync(ruleToSave2.getObjectID());
+    CompletableFuture<Rule> rule3Future = index.getRuleAsync(ruleToSave3.getObjectID());
+    CompletableFuture<Rule> rule4Future = index.getRuleAsync(ruleToSave4.getObjectID());
 
-    Rule retrievedRule = getRuleFuture.get();
-    Rule retrievedRule2 = getBatchedRuleFuture.get();
+    Rule retrievedRule = rule1Future.get();
+    Rule retrievedRule2 = rule2Future.get();
+    Rule retrievedRule3 = rule3Future.get();
+    Rule retrievedRule4 = rule4Future.get();
 
     assertThat(retrievedRule).usingRecursiveComparison().isEqualTo(ruleToSave);
-
     assertThat(retrievedRule2).usingRecursiveComparison().isEqualTo(ruleToSave2);
+    assertThat(retrievedRule3).usingRecursiveComparison().isEqualTo(ruleToSave3);
+    assertThat(retrievedRule4).usingRecursiveComparison().isEqualTo(ruleToSave4);
+
+    SearchResult<AlgoliaRule> searchWithContext =
+        index.searchAsync(new Query().setRuleContexts(Collections.singletonList("summer"))).get();
+
+    assertThat(searchWithContext.getHits()).hasSize(1);
 
     SearchResult<Rule> searchRules = index.searchRulesAsync(new RuleQuery("")).get();
-    assertThat(searchRules.getHits()).hasSize(2);
+    assertThat(searchRules.getHits()).hasSize(4);
 
     assertThat(
             searchRules.getHits().stream()
@@ -168,51 +194,56 @@ public abstract class QueryRulesTest {
     index.deleteRuleAsync(ruleToSave.getObjectID()).get().waitTask();
 
     SearchResult<Rule> searchRulesAfterDelete = index.searchRulesAsync(new RuleQuery("")).get();
-    assertThat(searchRulesAfterDelete.getHits()).hasSize(1);
+    assertThat(searchRulesAfterDelete.getHits()).hasSize(3);
     assertThat(searchRulesAfterDelete.getHits())
         .extracting(Rule::getObjectID)
-        .containsExactly(retrievedRule2.getObjectID());
+        .containsExactlyInAnyOrder(
+            retrievedRule2.getObjectID(),
+            retrievedRule3.getObjectID(),
+            retrievedRule4.getObjectID());
 
     index.clearRulesAsync().get().waitTask();
 
     SearchResult<Rule> searchRulesAfterClear = index.searchRulesAsync(new RuleQuery("")).get();
     assertThat(searchRulesAfterClear.getHits()).hasSize(0);
   }
+}
 
-  class AlgoliaRule {
+class AlgoliaRule {
 
-    AlgoliaRule(String objectID, String brand, String model) {
-      this.objectID = objectID;
-      this.brand = brand;
-      this.model = model;
-    }
+  public AlgoliaRule() {}
 
-    public String getObjectID() {
-      return objectID;
-    }
-
-    public void setObjectID(String objectID) {
-      this.objectID = objectID;
-    }
-
-    public String getBrand() {
-      return brand;
-    }
-
-    public void setBrand(String brand) {
-      this.brand = brand;
-    }
-
-    public String getModel() {
-      return model;
-    }
-
-    public void setModel(String model) {
-      this.model = model;
-    }
-
-    private String objectID;
-    private String brand;
-    private String model;
+  AlgoliaRule(String objectID, String brand, String model) {
+    this.objectID = objectID;
+    this.brand = brand;
+    this.model = model;
   }
+
+  public String getObjectID() {
+    return objectID;
+  }
+
+  public void setObjectID(String objectID) {
+    this.objectID = objectID;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  private String objectID;
+  private String brand;
+  private String model;
 }
