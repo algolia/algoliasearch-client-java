49f5295fad77c2126c4c1640542952366215cd28
Antoine Hémery <antoine.hemery@algolia.com>
2019-12-06 15:11:25 +0100

    update: run-travis.sh
    To make JDK8 test to run only apache module

--- .run-travis.sh
+++ .run-travis.sh
@@ -4,20 +4,21 @@ set -e
 
 if [[ "${JAVA_VERSION}" = "8" ]]; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [[ ! "$TRAVIS_PULL_REQUEST_SLUG" =~ ^algolia\/ ]]; then
-    eval $(./algolia-keys export) && mvn clean test;
+    # test only JDK8 comptaible requester
+    eval $(./algolia-keys export) && mvn clean compile -pl algoliasearch-apache,algoliasearch-core &&  mvn clean test -pl algoliasearch-apache,algoliasearch-core;
     exit $?
   else
-    mvn clean test;
+    mvn clean compile -pl algoliasearch-apache,algoliasearch-core && mvn clean test -pl algoliasearch-apache,algoliasearch-core;
     exit $?
   fi
 fi
 
 if [[ "${JAVA_VERSION}" = "11" ]]; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [[ ! "$TRAVIS_PULL_REQUEST_SLUG" =~ ^algolia\/ ]]; then
-    eval $(./algolia-keys export) && mvn clean test;
+    eval $(./algolia-keys export) && mvn clean compile && mvn clean test;
     exit $?
   else
-    mvn clean test;
+    mvn clean compile & mvn clean test;
     exit $?
   fi
 fi
