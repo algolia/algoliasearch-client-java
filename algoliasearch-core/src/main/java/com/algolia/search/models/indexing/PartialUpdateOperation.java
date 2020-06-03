949c83f5fdafaee921a6f5541128869c5e81ca4e
Anthony Seure <anthony.seure@gmail.com>
2020-06-03 11:03:00 +0200

    feat(search): expose built-in operations for partial updates with PartialUpdateOperation<T>

---
+++ algoliasearch-core/src/main/java/com/algolia/search/models/indexing/PartialUpdateOperation.java
@@ -0,0 +1,53 @@
+package com.algolia.search.models.indexing;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PartialUpdateOperation<T> {
+
+  @JsonProperty("_operation")
+  private String operation;
+
+  @JsonProperty("value")
+  private T value;
+
+  public PartialUpdateOperation(String operation, T value) {
+    this.operation = operation;
+    this.value = value;
+  }
+
+  public String getOperation() {
+    return operation;
+  }
+
+  public void setOperation(String operation) {
+    this.operation = operation;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
+  public void setValue(T value) {
+    this.value = value;
+  }
+
+  public static PartialUpdateOperation<Integer> increment(Integer value) {
+    return new PartialUpdateOperation<>(PartialUpdateOperationType.INCREMENT, value);
+  }
+
+  public static PartialUpdateOperation<Integer> decrement(Integer value) {
+    return new PartialUpdateOperation<>(PartialUpdateOperationType.DECREMENT, value);
+  }
+
+  public static <T> PartialUpdateOperation<T> add(T value) {
+    return new PartialUpdateOperation<>(PartialUpdateOperationType.ADD, value);
+  }
+
+  public static <T> PartialUpdateOperation<T> addUnique(T value) {
+    return new PartialUpdateOperation<>(PartialUpdateOperationType.ADD_UNIQUE, value);
+  }
+
+  public static <T> PartialUpdateOperation<T> remove(T value) {
+    return new PartialUpdateOperation<>(PartialUpdateOperationType.REMOVE, value);
+  }
+}
---
+++ algoliasearch-core/src/main/java/com/algolia/search/models/indexing/PartialUpdateOperationType.java
@@ -0,0 +1,9 @@
+package com.algolia.search.models.indexing;
+
+public class PartialUpdateOperationType {
+  public static final String INCREMENT = "Increment";
+  public static final String DECREMENT = "Decrement";
+  public static final String ADD = "Add";
+  public static final String ADD_UNIQUE = "AddUnique";
+  public static final String REMOVE = "Remove";
+}
