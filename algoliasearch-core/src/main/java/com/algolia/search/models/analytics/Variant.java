230fcd7477b4531a3d46f4a28dfea4b20404c1aa
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-19 11:15:27 +0200

    fix(ABTest): add missing fields in Variant

--- algoliasearch-core/src/main/java/com/algolia/search/models/analytics/Variant.java
+++ algoliasearch-core/src/main/java/com/algolia/search/models/analytics/Variant.java
@@ -19,8 +19,38 @@ public class Variant implements Serializable {
 
   // Properties available at construction time
   private String index;
+
+  // Percentage of the traffic that should be going to the variant. The sum of the percentage should
+  // be equal to 100.
   private int trafficPercentage;
+
+  // Description of the variant. This is useful when seing the results in the dashboard or via the
+  // API.
   private String description;
+
+  // Average click position for the variant.
+  private Float averageClickPosition;
+
+  // Distinct click count for the variant.
+  private Integer clickCount;
+
+  // Click through rate for the variant.
+  private Float clickThroughRate;
+
+  // Click through rate for the variant.
+  private Integer conversionCount;
+
+  // Distinct conversion count for the variant.
+  private Float conversionRate;
+
+  // No result count
+  private Integer noResultCount;
+
+  // Search count
+  private Integer searchCount;
+
+  // User Count
+  private Integer userCount;
 
   // Properties set by the AB Testing API
   private Query customSearchParameters;
@@ -63,6 +93,70 @@ public class Variant implements Serializable {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  public Float getAverageClickPosition() {
+    return averageClickPosition;
+  }
+
+  public void setAverageClickPosition(Float averageClickPosition) {
+    this.averageClickPosition = averageClickPosition;
+  }
+
+  public Integer getClickCount() {
+    return clickCount;
+  }
+
+  public void setClickCount(Integer clickCount) {
+    this.clickCount = clickCount;
+  }
+
+  public Float getClickThroughRate() {
+    return clickThroughRate;
+  }
+
+  public void setClickThroughRate(Float clickThroughRate) {
+    this.clickThroughRate = clickThroughRate;
+  }
+
+  public Integer getConversionCount() {
+    return conversionCount;
+  }
+
+  public void setConversionCount(Integer conversionCount) {
+    this.conversionCount = conversionCount;
+  }
+
+  public Float getConversionRate() {
+    return conversionRate;
+  }
+
+  public void setConversionRate(Float conversionRate) {
+    this.conversionRate = conversionRate;
+  }
+
+  public Integer getNoResultCount() {
+    return noResultCount;
+  }
+
+  public void setNoResultCount(Integer noResultCount) {
+    this.noResultCount = noResultCount;
+  }
+
+  public Integer getSearchCount() {
+    return searchCount;
+  }
+
+  public void setSearchCount(Integer searchCount) {
+    this.searchCount = searchCount;
+  }
+
+  public Integer getUserCount() {
+    return userCount;
+  }
+
+  public void setUserCount(Integer userCount) {
+    this.userCount = userCount;
   }
 
   @JsonProperty
