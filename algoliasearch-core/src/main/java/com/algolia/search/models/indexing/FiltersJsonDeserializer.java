99a40c3226b7e0a3ba61e4cfdda4fddcda1464ba
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-02-01 11:18:25 +0100

    apply code format

--- algoliasearch-core/src/main/java/com/algolia/search/models/indexing/FiltersJsonDeserializer.java
+++ algoliasearch-core/src/main/java/com/algolia/search/models/indexing/FiltersJsonDeserializer.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
