3a9a14f682e8b515f230f2b3c9f1226c31d28061
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-03-17 16:25:24 +0100

    workaround for TLSv1.3 and HTTP/2 with Java 11

--- algoliasearch-java-net/src/test/java/com/algolia/search/analytics/AnalyticsTest.java
+++ algoliasearch-java-net/src/test/java/com/algolia/search/analytics/AnalyticsTest.java
@@ -1,12 +1,14 @@
 package com.algolia.search.analytics;
 
-import static com.algolia.search.integration.TestHelpers.ALGOLIA_ADMIN_KEY_1;
-import static com.algolia.search.integration.TestHelpers.ALGOLIA_APPLICATION_ID_1;
-
 import com.algolia.search.*;
-import java.io.IOException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+
+import static com.algolia.search.integration.TestHelpers.ALGOLIA_ADMIN_KEY_1;
+import static com.algolia.search.integration.TestHelpers.ALGOLIA_APPLICATION_ID_1;
 
 @ExtendWith({IntegrationTestExtension.class})
 class AnalyticsTest extends com.algolia.search.integration.analytics.AnalyticsTest {
@@ -14,7 +16,11 @@ class AnalyticsTest extends com.algolia.search.integration.analytics.AnalyticsTe
   private static AnalyticsConfig analyticsConfig =
       new AnalyticsConfig.Builder(ALGOLIA_APPLICATION_ID_1, ALGOLIA_ADMIN_KEY_1).build();
   private static AnalyticsClient analyticsClient =
-      new AnalyticsClient(analyticsConfig, new JavaNetHttpRequester(analyticsConfig));
+      new AnalyticsClient(
+          analyticsConfig,
+          new JavaNetHttpRequester(
+              analyticsConfig,
+              HttpClient.newBuilder().sslParameters(SSLUtils.getDefaultSSLParameters())));
 
   AnalyticsTest() {
     super(IntegrationTestExtension.searchClient, analyticsClient);
--- algoliasearch-java-net/src/test/java/com/algolia/search/insights/InsightsTest.java
+++ algoliasearch-java-net/src/test/java/com/algolia/search/insights/InsightsTest.java
@@ -1,13 +1,12 @@
 package com.algolia.search.insights;
 
+import com.algolia.search.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.net.http.HttpClient;
+
 import static com.algolia.search.integration.TestHelpers.ALGOLIA_ADMIN_KEY_1;
 import static com.algolia.search.integration.TestHelpers.ALGOLIA_APPLICATION_ID_1;
-
-import com.algolia.search.InsightsClient;
-import com.algolia.search.InsightsConfig;
-import com.algolia.search.IntegrationTestExtension;
-import com.algolia.search.JavaNetHttpRequester;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({IntegrationTestExtension.class})
 class InsightsTest extends com.algolia.search.integration.insights.InsightsTest {
@@ -18,6 +17,10 @@ class InsightsTest extends com.algolia.search.integration.insights.InsightsTest 
   InsightsTest() {
     super(
         IntegrationTestExtension.searchClient,
-        new InsightsClient(config, new JavaNetHttpRequester(config)));
+        new InsightsClient(
+            config,
+            new JavaNetHttpRequester(
+                config,
+                HttpClient.newBuilder().sslParameters(SSLUtils.getDefaultSSLParameters()))));
   }
 }
