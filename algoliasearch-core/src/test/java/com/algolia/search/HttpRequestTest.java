879a9f2a8527778a92f429d2a53fbccff0f5194d
Mouaad Aallam <mouaad.aallam@algolia.com>
2023-05-31 10:16:05 +0200

    fix(core): http request timeout overflow (#785)

---
+++ algoliasearch-core/src/test/java/com/algolia/search/HttpRequestTest.java
@@ -0,0 +1,30 @@
+package com.algolia.search;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.algolia.search.models.HttpMethod;
+import com.algolia.search.models.HttpRequest;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+
+class HttpRequestTest {
+
+  @Test
+  void testIncrementTimeout() {
+    HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "/", Collections.emptyMap(), 1);
+
+    // Test normal case
+    httpRequest.incrementTimeout(1);
+    assertEquals(2, httpRequest.getTimeout()); // 1*(1+1) = 2
+
+    // Test large value
+    httpRequest.incrementTimeout(Integer.MAX_VALUE);
+    assertEquals(
+        Integer.MAX_VALUE,
+        httpRequest.getTimeout()); // Should have caused overflow and been set to MAX_VALUE
+
+    // Test timeout already at max value
+    httpRequest.incrementTimeout(1);
+    assertEquals(Integer.MAX_VALUE, httpRequest.getTimeout()); // Should not change timeout
+  }
+}
